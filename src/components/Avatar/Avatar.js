f03eec022723ca45e800854866cb31cc4ed00f97
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-19 18:35:14 -0400

    Change Avatar component to a function declaration

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -4,7 +4,7 @@ import useFocusStack from '../../hooks/useFocusStack'
 import Identicon from '../Identicon/Identicon'
 import styles from './Avatar.module.css'
 
-const Avatar = ({ username, image, size = 24 }) => {
+function Avatar({ username, image, size = 24 }) {
   const { openProfile } = useProfile()
   const { push } = useFocusStack()
   const avatarRef = useRef(null)
