0a24a77a4996bf9aea758780bdbd12b4088d4c0c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-19 16:51:16 -0400

    Integrate useFocusStack in NotificationsButton

--- src/components/MobileNav/NotificationsButton/NotificationsButton.js
+++ src/components/MobileNav/NotificationsButton/NotificationsButton.js
@@ -1,10 +1,13 @@
+import useFocusStack from '../../hooks/useFocusStack'
 import BellIcon from '../../Icons/BellIcon/BellIcon'
 import styles from './NotificationsButton.module.css'
 
 function NotificationsButton({ notificationsButtonRef, openNotifications }) {
-
+  const { push } = useFocusStack()
+  
   const action = () => {
     navigator.vibrate(50)
+    push(notificationsButtonRef.current)
     openNotifications()
   }
   
