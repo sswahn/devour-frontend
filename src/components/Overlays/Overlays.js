c0956f29138ad398e55f7229e9eae4b9d452a685
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-20 17:17:34 -0400

    Update Overlays.js

--- src/components/Overlays/Overlays.js
+++ src/components/Overlays/Overlays.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { createPortal } from 'react-dom'
 import { FocusTrapProvider } from '../Providers/FocusTrapProvider'
 import Authentication from '../../features/Authentication/Authentication'
+import Dashboard from '../../Dashboard/Dashboard'
 import SearchForm from '../SearchForm/SearchForm'
 import Camera from '../../features/Camera/Camera'
 import Notifications from '../Notifications/Notifications'
@@ -9,11 +10,13 @@ import Profile from '../Profile/Profile'
 
 function Overlays({ 
   authenticationIsOpen,
+  dashboardIsOpen,
   searchIsOpen, 
   cameraIsOpen, 
   notificationsIsOpen, 
   profileIsOpen, 
   closeAuthentication,
+  closeDashboard,
   closeSearch, 
   closeCamera, 
   closeNotifications, 
@@ -51,6 +54,7 @@ function Overlays({
   return createPortal(
     <FocusTrapProvider>
       {authenticationIsOpen && <Authentication closeAuthentication={closeAuthentication} />}
+      {dashboardIsOpen && <Dashboard closeDashboard={closeDashboard} />}
       {searchIsOpen && <SearchForm closeSearch={closeSearch} />}
       {cameraIsOpen && <Camera closeCamera={closeCamera} />}
       {notificationsIsOpen && <Notifications closeNotifications={closeNotifications} />}
