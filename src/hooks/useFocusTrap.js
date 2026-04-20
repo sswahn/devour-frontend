5fab343873ab5e9e9f20b18f8729fff1d68801b9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-20 10:44:52 -0400

    Refactor useFocusTrap to return overlayRef and focusRef

--- src/hooks/useFocusTrap.js
+++ src/hooks/useFocusTrap.js
@@ -2,13 +2,9 @@ import { useContext } from 'react'
 import { FocusTrapContext } from '../components/Providers/FocusTrapProvider'
 
 function useFocusTrap() {
-  const focusRef = useContext(FocusTrapContext)
+  const {overlayRef, focusRef} = useContext(FocusTrapContext)
   
-  if (!focusRef) {
-    // -->
-  }
-  
-  return focusRef
+  return {overlayRef, focusRef}
 }
 
 export default useFocusTrap
