fd22109a0feda765740ab256db8ce40c343f9dea
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-20 12:31:54 -0400

    Update useSwipeToClose.js

--- src/hooks/useSwipeToClose.js
+++ src/hooks/useSwipeToClose.js
@@ -46,6 +46,10 @@ function swipeToClose() {
     const deltaX = event.clientX - startX
     const isCorrectDir = activeSide === 'left' ? deltaX > 0 : deltaX < 0
     const CLOSE_THRESHOLD = 150 
+
+    console.log('startX: ', startX)
+    console.log('deltaX: ', deltaX)
+    console.log('isCorrectDir: ', isCorrectDir)
     
     if (Math.abs(deltaX) > CLOSE_THRESHOLD && isCorrectDir) {
       console.log('overlay.method() fired! : ', overlay.method)
