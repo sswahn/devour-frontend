5da31cc3685732a3a0008cc6e7f0dc430cac1118
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-20 17:40:13 -0400

    Create VideoOverlay.js

---
+++ src/features/Feed/VideoOverlay/VideoOverlay.js
@@ -0,0 +1,11 @@
+import styles from './VideoOverlay.module.css'
+
+function VideoOverlay() {
+
+  return (
+    <div className={styles.videoOverlay}>
+    </div>
+  )
+}
+
+export default VideoOverlay
