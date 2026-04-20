ede519de19a5e793ce02962d31741b29a0c1c44f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-20 10:34:13 -0400

    Update useSwipeToClose.js

--- src/hooks/useSwipeToClose.js
+++ src/hooks/useSwipeToClose.js
@@ -1,12 +1,49 @@
 import { useEffect } from 'react'
 
-function useGestures() {
+function swipeToClose() {
 
-  const swipeToClose = (element, methods) => {
-    
+  const resetSwipeData = () => {
+    swipeData.current = { 
+      startX: 0, 
+      activeSide: null
+    }
   }
 
+  const onPointerDown = event => {
+    const { clientX, pointerId, currentTarget } = event
+    const width = window.innerWidth
+    const EDGE_THRESHOLD = 30
+    
+    // Only capture if actually hitting an edge
+    const isLeft = clientX < EDGE_THRESHOLD
+    const isRight = clientX > width - EDGE_THRESHOLD
+    if (isLeft || isRight) {
+      currentTarget.setPointerCapture(pointerId)
+      swipeData.current = { 
+        startX: clientX, 
+        activeSide: isLeft ? 'left' : 'right'
+      }
+    }
+  }
 
+  const onPointerUp = event => {
+    const { activeSide, startX } = swipeData.current
+    if (!activeSide) {
+      return
+    }
+    const deltaX = event.clientX - startX
+    const isCorrectDir = activeSide === 'left' ? deltaX > 0 : deltaX < 0
+    const CLOSE_THRESHOLD = 150 
+    
+    if (Math.abs(deltaX) > CLOSE_THRESHOLD && isCorrectDir) {
+      action()
+    }
+    resetSwipeData()
+  }
+  
+  const onPointerCancel = event => {
+    resetSwipeData()
+  }
   
   useEffect(() => {
     element.addEventListener('onPointerUp', methods.up)
@@ -17,9 +54,7 @@ function useGestures() {
     }
   }, [])
 
-  return {
-    swipeToClose
-  }
+  
 }
 
-export default useGestures
+export default swipeToClose
