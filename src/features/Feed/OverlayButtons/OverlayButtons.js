9d18936396c55cdb0c0af44e2aecfbc0cd374a2e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-20 17:50:02 -0400

    Update and rename VideoOverlay.js to OverlayButtons.js

---
+++ src/features/Feed/OverlayButtons/OverlayButtons.js
@@ -0,0 +1,27 @@
+import styles from './VideoOverlay.module.css'
+import CloseButton from '../../../components/CloseButton/CloseButton'
+import Dropdown from '../../../components/Dropdown/Dropdown'
+import LikeButton from '../LikeButton/LikeButton'
+import CommentsButton from '../CommentsButton/CommetsButton'
+import ShareButton from '../ShareButton/ShareButton'
+import FullscreenButton from '../FullscreenButton/FullscreenButton'
+
+function VideoOverlay({ closeFeed }) {
+
+  return (
+    <nav className={styles.videoOverlay}>
+      <div> {/* top of node header */}
+        {isFullscreen && <CloseButton overlay="feed overlay" close={closeFeed} />}
+        <Dropdown />
+      </div>
+      <div> {/* sidebar right */}
+        <LikeButton />
+        <CommentsButton />
+        <ShareButton />
+        <FullscreenButton />
+      </div>
+    </nav>
+  )
+}
+
+export default VideoOverlay
