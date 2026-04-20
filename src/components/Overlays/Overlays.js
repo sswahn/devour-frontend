75d7df3ed51108ba4cece379e2902d805bb832e5
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-20 09:58:02 -0400

    Update Overlays.js

--- src/components/Overlays/Overlays.js
+++ src/components/Overlays/Overlays.js
@@ -42,9 +42,9 @@ function Overlays({
   }
   
   useEffect(() => {
-    document.addEventListener('keydown', closeOverlay)
+   // document.addEventListener('keydown', closeOverlay)
     return () => {
-      document.removeEventListener('keydown', closeOverlay)
+     // document.removeEventListener('keydown', closeOverlay)
     }
   }, [])
 
