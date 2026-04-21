ccc93e63ed337976b9c31410639bfafbb29a880c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-21 12:44:35 -0400

    Update SideNav.js

--- src/features/Feed/SideNav/SideNav.js
+++ src/features/Feed/SideNav/SideNav.js
@@ -4,13 +4,13 @@ import ShareButton from '../ShareButton/ShareButton'
 import FullscreenButton from '../FullscreenButton/FullscreenButton'
 import styles from './SideNav.module.css'
 
-function SideNav({ doubleTap }) {
+function SideNav({ doubleTap, longPress }) {
   
   return (
     <div className={styles.sideNav}>
       <LikeButton doubleTap={doubleTap} />
       <CommentsButton />
-      <ShareButton />
+      <ShareButton longPress={longPress} />
       <FullscreenButton />
     </div>
   )
