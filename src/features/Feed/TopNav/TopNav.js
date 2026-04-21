0a920e1ceba117ae61a000de03ffd764e9ece403
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-21 09:35:09 -0400

    Update TopNav.js

--- src/features/Feed/TopNav/TopNav.js
+++ src/features/Feed/TopNav/TopNav.js
@@ -1,9 +1,10 @@
 import CloseButton from '../../../components/CloseButton/CloseButton'
 import Dropdown from '../../../components/Dropdown/Dropdown'
+import styles from './TopNav.module.css'
 
 function TopNav() {
   return (
-    <div>
+    <div className={styles.topNav}>
       {/* isFullscreen && <CloseButton overlay="feed overlay" close={closeFeed} /> */}
       <Dropdown items={[1,2,3]} />
     </div>
