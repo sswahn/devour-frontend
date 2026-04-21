f46892fb6dfbcd31f20909aa9e6eb0469def7bdf
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-21 09:32:58 -0400

    Create TopNav.js

---
+++ src/features/Feed/TopNav/TopNav.js
@@ -0,0 +1,11 @@
+
+function TopNav() {
+  return (
+    <div>
+      {/* isFullscreen && <CloseButton overlay="feed overlay" close={closeFeed} /> */}
+      <Dropdown items={[1,2,3]} />
+    </div>
+  )
+}
+
+export default TopNav
