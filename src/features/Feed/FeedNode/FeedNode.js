64e86ba12ef3adc01c8fdfb60eeff30a45fcf8f0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-21 11:04:59 -0400

    Update FeedNode.js

--- src/features/Feed/FeedNode/FeedNode.js
+++ src/features/Feed/FeedNode/FeedNode.js
@@ -2,16 +2,15 @@ import { useState, useRef } from 'react'
 import SideNav from '../SideNav/SideNav'
 
 function FeedNode({ item, index, count }) {
-  const [doubleTap, setDoubleTap] = useState(false)
+  const [toggleLike, setToggleLike] = useState(false)
   const prevTime = useRef(0)
   
   const doubleClick = event => {
     const now = performance.now()
     const deltaT = now - prevTime.current
     if (deltaT < 300) {
-      setDoubleTap(true)
+      setToggleLike(prev => !prev)
     } 
-    setDoubleTap(false)
     prevTime.current = now
   }
 
