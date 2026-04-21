7e40b1567d054b35021d7cf0dad19c05e05c6022
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-21 10:41:39 -0400

    Update FeedNode.js

--- src/features/Feed/FeedNode/FeedNode.js
+++ src/features/Feed/FeedNode/FeedNode.js
@@ -9,8 +9,10 @@ function FeedNode({ item, index, count }) {
     const deltaT = now - prevTime.current
     if (deltaT < 300) {
       // perform doubleTap function (like)
+      // reset prevTime.current = 0
+    } else {
+      prevTime.current = now
     }
-    prevTime.current = now
   }
 
     // all gestures go here. eventually abstracted to hooks, using gestrue engine.
