f5575c6e19e434187e84469e61897c7e4e1c7452
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-20 20:01:16 -0400

    Update ShareButton.js

--- src/features/Feed/ShareButton/ShareButton.js
+++ src/features/Feed/ShareButton/ShareButton.js
@@ -2,18 +2,17 @@ import ShareIcon from '../../../components/Icons/ShareIcon/ShareIcon'
 import styles from './ShareButton.module.css'
 
 function ShareButton({ data }) {
-  const shareData = {
-    title: data.title || 'Check this out!',
-    text: data.text || 'I found an amazing developer guide.',
-    url: data.url || 'https://developer.mozilla.org',
-    files: data.files || [
-      new File([blob], 'snapshot.png', { type: 'image/png' })
-    ]
-  }
 
   const action = () => {
     navigator.vibrate(50)
-    navigator.share(shareData)
+    navigator.share({
+      title: data.title || 'Check this out!',
+      text: data.text || 'I found this video.',
+      url: data.url || 'https://sswahn.github.io/devour-frontend',
+      files: data.files || [
+        new File([blob], 'snapshot.png', { type: 'image/png' })
+      ]
+    })
   }
   
   const onClick = event => {
