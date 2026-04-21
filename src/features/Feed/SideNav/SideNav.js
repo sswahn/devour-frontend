b6361c0a65d2bfcd2c0994b2b8ebae9f48014245
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-21 09:29:01 -0400

    Update SideNav.js

--- src/features/Feed/SideNav/SideNav.js
+++ src/features/Feed/SideNav/SideNav.js
@@ -1,3 +1,8 @@
+import LikeButton from '../LikeButton/LikeButton'
+import CommentsButton from '../CommentsButton/CommentsButton'
+import ShareButton from '../ShareButton/ShareButton'
+import FullscreenButton from '../FullscreenButton/FullscreenButton'
+import styles from './SideNav.module.css'
 
 function SideNav() {
   
