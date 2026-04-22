0a26bc2b4d83a085f6afca67879ff300cf954b76
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-22 17:46:48 -0400

    Update useDoubleTap.js

--- src/hooks/useDoubleTap.js
+++ src/hooks/useDoubleTap.js
@@ -1,6 +1,19 @@
 
 function useDoubleTap() {
 
+
+  const doubleTapOnUp = () => {
+    const now = performance.now()
+    const deltaT = now - lastTapTime.current
+    if (deltaT > 0 && deltaT < doubleTapDelay) {
+      setDoubleTap(now)
+      lastTapTime.current = 0
+    } else {
+      setTap(now)
+      lastTapTime.current = now
+    }
+  }
+  
   return (
     
   )
