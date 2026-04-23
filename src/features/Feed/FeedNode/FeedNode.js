fefa6e3dc22151542d0c2aa59b7ce68add06e8b7
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-23 08:41:44 -0400

    Update FeedNode.js

--- src/features/Feed/FeedNode/FeedNode.js
+++ src/features/Feed/FeedNode/FeedNode.js
@@ -72,16 +72,17 @@ function FeedNode({ item, index, count }) {
   
   // this tabIndex etc. breaks the natural flow of the page, header gets skipped...
   return (
-    <article  // consider using figure instead of article.
-      {...handlers}
+    <figure 
+      style={{ background: '#666', borderRadius: '10px', height: '100%', width: '100%' }}
+      {...handlers} 
       tabIndex={index} aria-posinset={index} aria-setsize={count}>
       <TopNav />
-      <figure style={{ background: '#666', borderRadius: '10px', height: '100%', width: '100%' }}>
-        {/* data.videoUrl && <video ref={ref} src={data.videoUrl} preload="metadata" muted playsInline loop /> */}
-        {/* data.caption ?? <figcaption>{data.caption}</figcaption> */}
-      </figure>
+        
+      {/* data.videoUrl && <video ref={ref} src={data.videoUrl} preload="metadata" muted playsInline loop /> */}
+      {/* data.caption ?? <figcaption>{data.caption}</figcaption> */}
+
       <SideNav doubleTap={doubleTap} longPress={longPress} />
-    </article>
+    </figure>
   )
 }
 
