1be6e1812b844e939cb7b40f330e38732a188031
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-23 11:38:07 -0400

    Update CameraProvider.js

--- src/components/Providers/CameraProvider.js
+++ src/components/Providers/CameraProvider.js
@@ -1,18 +1,15 @@
 import { useState, createContext } from 'react'
 
-const GetCameraContext = createContext(null)
-const SetCameraContext = createContext(null)
+const CameraContext = createContext(null)
 
 function CameraProvider({ children }) {
   const [state, setState] = useState(false)
 
   return (
-    <GetCameraContext.Provider value={state}>
-      <SetCameraContext.Provider value={setState}>
-        {children}
-      </SetCameraContext.Provider>
-    </GetCameraContext.Provider>
+    <CameraContext.Provider value={state, setState}>
+      {children}
+    </CameraContext.Provider>
   )
 }
 
-export { GetCameraContext, SetCameraContext, CameraProvider }
+export { CameraContext, CameraProvider }
