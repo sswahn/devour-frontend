633cbbfbb3fa2739c9e7de3733ba51e33bbe0e11
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-23 17:13:04 -0400

    Update EditInput.js

--- src/components/Profile/EditInput/EditInput.js
+++ src/components/Profile/EditInput/EditInput.js
@@ -4,12 +4,20 @@ import styles from './EditInput.module.css'
 function EditInput({ field, value }) {
   const [inputValue, setInputValue] = useState('')
   
-  const onChange = event => {
-    setInputValue(event.target.value)  
+  const onKeyDown = event => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      
+    // make request
+    // update profile data,
+    // close input
+    // not necessarily in that order.
+      
+    }
   }
   
   return (
-    <input className={styles.editInput} onChange={onChange} type="text" placeholder={value} aria-label={`${field} input`} />
+    <input className={styles.editInput} onKeyDown={onKeyDown} type="text" placeholder={value} aria-label={`${field} input`} />
   )
 }
 
