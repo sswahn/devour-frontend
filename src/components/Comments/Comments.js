dff2637172083dd0b709d95346554b414c598b9a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-23 13:11:45 -0400

    Implement comments form and display functionality

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,12 +1,31 @@
 
+import styles from './Comments.module.css'
 
 function Commets() {
 
   // Comments Form
   // Comments Feed
 
+  const onClick = event => {
+    navigator.vibrate?.(50)
+  }
+
+  const onSubmit = event => {
+    
+  }
+  
   return (
-    <></>
+    <section>
+      <form className={styles.comments} onSubmit={onSubmit} aria-label="comment form">
+        <textarea></textarea>
+        <button onClick={onClick} type="submit">Submit</button>
+      </form>
+      <div>
+        {data.comments.map(comment => 
+          <div key={comment.id}>{comment}</div>            
+        )}
+      </div>
+    </section>
   )
 }
 
