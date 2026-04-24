af9cd879aac0e69ecbc748c33582d37dd7afeaa5
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 13:55:30 -0400

    Refactor useSwipe hook variable names and logic

--- src/hooks/useSwipe.js
+++ src/hooks/useSwipe.js
@@ -1,27 +1,48 @@
 import { useState, useRef, useEffect } from 'react'
 
 function useSwipe() {
-  const swipeData = useRef({ 
+  const data = useRef({ 
     startX: 0,
     startY: 0,
     activeSide: null,
     pointerId: null,
     direction: null
   })
+
+  const reset = currentTarget => {
+    const { id } = data.current
+    if (currentTarget.hasPointerCapture(id)) {
+      currentTarget.releasePointerCapture(id)
+    }
+    data.current = {}
+  }
+  
   const onSwipeDown = event => {
-    
+    const { clientX, clientY, pointerId } = event
+    data.current = {
+      startX: clientX,
+      startY: clientY,
+      id: pointerId,
+    }
+    currentTarget.setPointerCapture(pointerId)
   }
   
   const onSwipeMove = event => {
-    
+    if (id !== pointerId) { 
+      return
+    }
   }
   
   const onSwipeUp = event => {
-    
+    if (id !== pointerId) { 
+      return
+    }
   }
   
   const onSwipeCancel = event => {
-    
+    if (id !== pointerId) { 
+      return
+    }
   }
 
   return {
