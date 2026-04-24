16cd25e8268080da5d942764e7de20d8e55fa1a5
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 07:25:56 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,16 @@
 
 export const overlays = {
   authentication: 'authentication',
-  dashboard: 'dashboard',
-  search: 'search',
   camera: 'camera',
+  dashboard: 'dashboard',
   notifications: 'notifications',
   profile: 'profile',
+  search: 'search'
 }
 
 export const apis = {
-  
+  login: '/api/v1/login',
+  logout: '/api/v1/logout',
+  register: '/api/v1/register',
+  session: '/api/v1/session'
 }
