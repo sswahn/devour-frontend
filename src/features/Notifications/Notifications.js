44c9763f78659a271d73234c3360ba9c443cf9d1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 07:54:05 -0400

    Import overlays from config in Notifications.js

--- src/features/Notifications/Notifications.js
+++ src/features/Notifications/Notifications.js
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from 'react'
+import { overlays } from '../../config'
 import useFocusStack from '../../hooks/useFocusStack'
 import useFocusTrap from '../../hooks/useFocusTrap'
 import EllipsisVerticalIcon from '../Icons/EllipsisVerticalIcon/EllipsisVerticalIcon'
