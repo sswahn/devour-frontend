895eabe77f0760e20ecd7de2678b528db90903bc
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 13:35:03 -0400

    Create useGestures.js

---
+++ src/hooks/useGestures.js
@@ -0,0 +1,29 @@
+
+
+function useGesture() {
+
+  const onGestureDown = event => {
+    
+  }
+  
+  const onGestureMove = event => {
+    
+  }
+  
+  const onGestureUp = event => {
+    
+  }
+  
+  const onGestureCancel = event => {
+    
+  }
+
+  return {
+    onGestureDown,
+    onGestureMove,
+    onGestureUp,
+    onGestureCancel
+  }
+}
+
+export default useGesture
