e9f937d00e73843dbc2553fcc1c0b981a9d1ac13
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 10:45:36 -0400

    Add ProfileProvider to Providers component

--- src/Providers.js
+++ src/Providers.js
@@ -2,6 +2,8 @@ import { SessionProvider } from './components/Providers/SessionProvider'
 import { FocusTrapProvider } from './components/Providers/FocusTrapProvider'
 import { FocusStackProvider } from './components/Providers/FocusStackProvider'
 import { OverlayProvider } from './components/Providers/OverlayProvider'
+import { ProfileProvider } from './components/Providers/ProfileProvider'
+
 
 function Providers({ children }) {
   return (
@@ -9,7 +11,9 @@ function Providers({ children }) {
       <FocusTrapProvider>
         <FocusStackProvider>
           <OverlayProvider>
-            {children}
+            <ProfileProvider>
+              {children}
+            </ProfileProvider>
           </OverlayProvider>
         </FocusStackProvider>
       </FocusTrapProvider>
