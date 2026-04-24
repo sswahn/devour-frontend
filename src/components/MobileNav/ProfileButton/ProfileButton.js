0288c7abff00fd074d59bec8a5a00674df85e47d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 10:01:24 -0400

    Update ProfileButton.js

--- src/components/MobileNav/ProfileButton/ProfileButton.js
+++ src/components/MobileNav/ProfileButton/ProfileButton.js
@@ -1,17 +1,23 @@
 import { useRef } from 'react'
+import { overlays } from '../../../config'
 import useSession from '../../../hooks/useSession'
 import useFocusStack from '../../../hooks/useFocusStack'
+import useProfile from '../../../hooks/useProfile'
+import useOverlay from '../../../hooks/useOverlay'
 import UserIcon from '../../Icons/UserIcon/UserIcon'
 import styles from './ProfileButton.module.css'
 
-function ProfileButton({ openProfile }) {
+function ProfileButton() {
   const buttonRef = useRef(null)
   const { session } = useSession()
   const { push } = useFocusStack()
+  const { setUserProfile } = useProfile()
+  const { openOverlay } = useOverlay()
   
   const action = async () => {
     push(buttonRef.current)
-    openProfile(session.username)
+    setUserProfile(session.username)
+    openOverlay(overlay.profile)
   }
   
   const onClick = event => {
