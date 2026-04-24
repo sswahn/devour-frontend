7057b7fd196e749a34507e32a80f2dc02835b924
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 09:52:27 -0400

    Update Providers.js

--- src/Providers.js
+++ src/Providers.js
@@ -1,13 +1,16 @@
 import { SessionProvider } from './components/Providers/SessionProvider'
 import { FocusTrapProvider } from './components/Providers/FocusTrapProvider'
 import { FocusStackProvider } from './components/Providers/FocusStackProvider'
+import { OverlayProvider } from './components/Providers/OverlayProvider'
 
 function Providers({ children }) {
   return (
     <SessionProvider>
       <FocusTrapProvider>
         <FocusStackProvider>
-          {children}
+          <OverlayProvider>
+            {children}
+          </OverlayProvider>
         </FocusStackProvider>
       </FocusTrapProvider>
     </SessionProvider>
