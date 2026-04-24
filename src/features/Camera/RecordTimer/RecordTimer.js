73f3e1316d04bddd325fb985a1f58d2bc8b52989
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 10:20:22 -0400

    Update RecordTimer.js

--- src/features/Camera/RecordTimer/RecordTimer.js
+++ src/features/Camera/RecordTimer/RecordTimer.js
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from 'react'
-import { Context } from '../../../Provider'
+import { Context } from '../../../archive/Provider'
 import database from '@sswahn/database'
 import styles from './recordtimer.module.css'
 
