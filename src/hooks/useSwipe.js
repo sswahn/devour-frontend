0aad79492d108558d95fc9433c7c80a3cc48ac3d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 13:44:54 -0400

    Fix missing newline at end of useSwipe.js
    Add a newline at the end of the file for consistency.

---
+++ src/hooks/useSwipe.js
@@ -0,0 +1,29 @@
+
+
+function useSwipe() {
+
+  const onSwipeDown = event => {
+    
+  }
+  
+  const onSwipeMove = event => {
+    
+  }
+  
+  const onSwipeUp = event => {
+    
+  }
+  
+  const onSwipeCancel = event => {
+    
+  }
+
+  return {
+    onSwipeDown,
+    onSwipeMove,
+    onSwipeUp,
+    onSwipeCancel
+  }
+}
+
+export default useSwipe
