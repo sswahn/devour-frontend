c6c02e4a1c208f3fc576566cf3073e5af8c2670c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 07:43:09 -0400

    Update Interface.js

--- src/components/Interface/Interface.js
+++ src/components/Interface/Interface.js
@@ -17,21 +17,18 @@ function Interface() {
   const [cameraIsOpen, setCameraIsOpen] = useState(false)
   const [notificationsIsOpen, setNotificationsIsOpen] = useState(false)
   */
+
+  const closeOverlay = () => setIsActive(undefined)
   
   const openAuthentication = () => setIsActive(overlays.authentication)
-  const closeAuthentication = () => setIsActive(undefined)
 
   const openDashboard = () => setIsActive(overlays.dashboard)
-  const closeDashboard = () => setIsActive(undefined)
   
   const openSearch = () => setIsActive(overlays.search)
-  const closeSearch = () => setIsActive(undefined)
 
   const openCamera = () => setIsActive(overlays.camera)
-  const closeCamera = () => setIsActive(undefined)
 
   const openNotifications = () => setIsActive(overlays.notifications)
-  const closeNotifications = () => setIsActive(undefined)
 
   const handleProfileContext = () => {
     profileIsOpen ? setIsActive(overlays.profile) : setIsActive(undefined)
@@ -56,20 +53,8 @@ function Interface() {
       />
       <Suspense fallback={<LoadingSpinner />}>
         <Overlays 
-/*
-          authenticationIsOpen={authenticationIsOpen}
-          dashboardIsOpen={dashboardIsOpen}
-          searchIsOpen={searchIsOpen} 
-          cameraIsOpen={cameraIsOpen}
-          notificationsIsOpen={notificationsIsOpen}
-          profileIsOpen={profileIsOpen}
-*/
           isActive={isActive}
-          closeAuthentication={closeAuthentication}
-          closeSearch={closeSearch}
-          closeCamera={closeCamera}
-          closeNotifications={closeNotifications}
-          closeProfile={closeProfile}
+          closeOverlay={closeOverlay}
         />
       </Suspense>
     </>
