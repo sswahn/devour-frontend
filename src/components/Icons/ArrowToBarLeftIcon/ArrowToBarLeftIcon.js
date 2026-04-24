f6eaa2df075799d9a7f127f9b4fe97cbf3c61964
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 12:50:22 -0400

    Refactor ArrowToBarLeftIcon to use props for size

--- src/components/Icons/ArrowToBarLeftIcon/ArrowToBarLeftIcon.js
+++ src/components/Icons/ArrowToBarLeftIcon/ArrowToBarLeftIcon.js
@@ -1,6 +1,6 @@
-const ArrowToBarLeftIcon = () => {
+function ArrowToBarLeftIcon({ size = 24 }) {
   return (
-    <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="arrow to bar left icon">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} aria-hidden="true">
       <path d="M4 4V20M8 12H20M8 12L12 8M8 12L12 16" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
