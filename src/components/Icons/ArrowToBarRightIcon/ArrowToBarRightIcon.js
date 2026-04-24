e15164192a5ec078b3213031b1cfbdb4e0ec342a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 12:51:40 -0400

    Refactor ArrowToBarRightIcon to accept size prop

--- src/components/Icons/ArrowToBarRightIcon/ArrowToBarRightIcon.js
+++ src/components/Icons/ArrowToBarRightIcon/ArrowToBarRightIcon.js
@@ -1,6 +1,6 @@
-const ArrowToBarRightIcon = () => {
+function ArrowToBarRightIcon({ size = 24 }) {
   return (
-    <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="arrow to bar right icon">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} aria-hidden="true">
       <path d="M20 4V20M4 12H16M16 12L12 8M16 12L12 16" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
