652c05a099973e76819e8db4792e928ad5374264
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 07:23:52 -0400

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+export const overlays = {
+  authentication: 'authentication',
+  dashboard: 'dashboard',
+  search: 'search',
+  camera: 'camera',
+  notifications: 'notifications',
+  profile: 'profile',
+}
+
+export const apis = {
+  
+}
