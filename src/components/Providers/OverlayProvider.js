0d29be715623169d99132d793577f8a6c14cef29
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 09:51:26 -0400

    Update OverlayProvider.js

--- src/components/Providers/OverlayProvider.js
+++ src/components/Providers/OverlayProvider.js
@@ -1,31 +1,20 @@
-import { useState, createContext } from 'react'
+import { useState, useCallback, createContext } from 'react'
 
 const OverlayContext = createContext(null)
 
 function OverlayProvider({ children }) {
   const [isActive, setIsActive] = useState(undefined)
 
-  const openOverlay = id => {
+  const openOverlay = useCallback(id => {
     setIsActive(id)
-  } 
+  }, []) 
   
-  const closeOverlay = () => {
+  const closeOverlay = useCallback(() => {
     setIsActive(undefined)
-  }
-
-    // create an overlays provider
-  // all this goes into it
-  // components can pull open overlay and close overlay from it
-  // it can use Seperate context providers to avoid rerenders
-  // it can use separate hooks to avoid rerenders useOpenOverlay, useCloseOverlay
-  // no more prop drilling for overlay controls.
-  // all these functions/logic goes away, and into the provider.
-  // profile issue then solved as:
-  // in avatar openOverlay(id), setProfileUser(username) (not sure where setProileUser comes from...)
-
+  }, [])
 
   return (
-    <OverlayContext.Provider value={{ state, setState }}>
+    <OverlayContext.Provider value={{ isActive, openOverlay, closeOverlay }}>
       {children}
     </OverlayContext.Provider>
   )
