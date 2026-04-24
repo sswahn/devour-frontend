c4b745fb14d6ab794a4b8c64ecdb9e8f5ee60e12
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-24 10:17:35 -0400

    Update Feed.js

--- src/features/Feed/Feed.js
+++ src/features/Feed/Feed.js
@@ -1,65 +1,20 @@
 import { useState, useRef, memo } from 'react'
-import { config } from '../../config'
+import { api } from '../../config'
 import server from '../../utilities/server'
-import database from '@sswahn/database'
+import database from '@sswahn/database' // move locally
 import FeedNode from './FeedNode/FeedNode'
 import Sentinel from './Sentinel/Sentinel' 
 import styles from './Feed.module.css'
 
 function Feed() {
-  const [data, setData] = useState([1])
-  const [batchNumber, setBatchNumber] = useState(0)
-  const [loading, setLoading] = useState(false)
-
-  const loadMoreData = async event => {
-    const response = await server.get(`${config.api.feed}/${batchNumber}`)
-    setBatchNumber(response.message.batchNumber)
-    setData({ ...data, ...response.message })
-  }
-
-
-  // Use smart caching, should be part of server.get()
-  const loadFromStorage = async () => {
-    const db = database()
-    const video = await db.get('video')
-    
-    alert(JSON.stringify(video?.video))
-    setData(video?.video)
-  }
-
-  // must be a button to open fullscreen
-  // add it to feedNode overlay nav
-  
-  const handleNodeClick = async event => {
-    try {
-      await (document.documentElement.requestFullscreen || document.documentElement.webkitRequestFullscreen)?.()
-      await screen.orientation.lock('portrait')
-    } catch (error) {
-      console.error(error)
-    }
-  }
 
   return (
-    <section className={styles.feed} role="feed" aria-busy={loading}>
-    {/*
-      <div style={{
-        background: '#777',
-        color: 'white',
-        height: '50dvh',
-        width: '100dvw',
-        maxWidth: '500px',
-        paddingTop: '100px',
-        margin: '0 auto 16px auto'
-      }}>suggestions</div>
-    */}
+    <section className={styles.feed} role="feed">
     
       {data.map((item, index) => 
         <FeedNode key={index} item={item} index={index + 1} count={data.length} />
       )}
 {/*
-      {data.map((item, index) => {
-        <FeedNode key={item.id} item={item} index={idex + 1} count={items.length} />
-      })}
       <Sentinel onVisible={loadMoreData} />
 */}
     </section>
