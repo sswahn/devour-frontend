9d80e527f28210bf221b4b598d8de5c7e3207317
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-26 09:35:39 -0400

    Fix import path for FocusStackContext

--- src/archive/hooks/useFocusStack.js
+++ src/archive/hooks/useFocusStack.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { FocusStackContext } from '../components/Providers/FocusStackProvider'
+import { FocusStackContext } from '../archive/Providers/FocusStackProvider'
 
 function useFocusStack() {
   const { push, pop } = useContext(FocusStackContext)
