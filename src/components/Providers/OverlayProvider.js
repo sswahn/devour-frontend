604734f145c1b5457c8ebf31693f4a5398ba9912
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-26 07:50:39 -0400

    Integrate useFocusStack into OverlayProvider

--- src/components/Providers/OverlayProvider.js
+++ src/components/Providers/OverlayProvider.js
@@ -1,16 +1,20 @@
 import { useState, useCallback, createContext } from 'react'
+import useFocusStack from '../../hooks/useFocusStack'
 
 const OverlayContext = createContext(null)
 
 function OverlayProvider({ children }) {
   const [isActive, setIsActive] = useState(undefined)
+  const { push, pop } = useFocusStack()
 
   const openOverlay = useCallback(id => {
+    push(id)
     setIsActive(id)
   }, []) 
   
   const closeOverlay = useCallback(() => {
     setIsActive(undefined)
+    pop()
   }, [])
 
   return (
