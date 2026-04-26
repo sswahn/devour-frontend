1d9ea15cca512a7c63ec38a5258006fe602bc418
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-26 14:26:54 -0400

    Update useSwipeFromEdge.js

--- src/hooks/useSwipeFromEdge.js
+++ src/hooks/useSwipeFromEdge.js
@@ -1,5 +1,5 @@
 import { useRef } from 'react'
-import useGestures from '../../hooks/useGestures'
+import useGestures from './useGestures'
 
 function useSwipeFromEdge(callback) {
   const ticking = useRef(false)
