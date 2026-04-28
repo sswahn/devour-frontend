4270d0d02d1c7984cd32d5e6adedb49aa81ff25a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-28 13:34:35 -0400

    Update Notifications.js

--- src/features/Notifications/Notifications.js
+++ src/features/Notifications/Notifications.js
@@ -94,12 +94,9 @@ function Notifications() {
       return
     }
     const raw = deltaY < 0 ? deltaY : 0 
-    const resisted = raw / (1 + Math.abs(raw) / 300)
+    const translateY = raw / (1 + Math.abs(raw) / 300)
     const newHeight = latestHeight.current + Math.abs(deltaY) 
-    if (newHeight > window.innerHeight) {
-      return
-    }
-    throttleTransition(resisted, newHeight, currentTarget)
+    throttleTransition(translateY, newHeight, currentTarget)
   }
   
   const onPointerUp = event => {
