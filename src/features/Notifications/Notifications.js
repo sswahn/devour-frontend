7f8383619b9bb19108ca3d74a8e8371a743730fe
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-28 14:36:15 -0400

    Update Notifications.js

--- src/features/Notifications/Notifications.js
+++ src/features/Notifications/Notifications.js
@@ -93,10 +93,12 @@ function Notifications() {
     if (deltaY === undefined || axis === 'x') {
       return
     }
-    const raw = deltaY < 0 ? deltaY : 8
-    const translateY = raw / (1 + Math.abs(raw) / 300)
-    const newHeight = latestHeight.current + Math.abs(deltaY) 
-    throttleTransition(translateY, newHeight, currentTarget)
+    // const raw = deltaY < 0 ? deltaY : 8
+    // const translateY = raw / (1 + Math.abs(raw) / 300)
+    // const newHeight = latestHeight.current + Math.abs(deltaY) 
+
+    const clampedY = Math.max(deltaY, 8) 
+    throttleTransition(clampedY, newHeight, currentTarget)
   }
   
   const onPointerUp = event => {
