5b0059c07922d323ebd64f40d5a6e319c67fde19
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-28 11:13:21 -0400

    Update useSwipeFromEdge.js

--- src/hooks/useSwipeFromEdge.js
+++ src/hooks/useSwipeFromEdge.js
@@ -38,8 +38,8 @@ function useSwipeFromEdge(callback) {
   
   const onPointerMove = event => {
     const { currentTarget } = event
-    const { deltaX, edge, direction } = onGestureMove(event)
-    if (deltaX === undefined || direction === 'y') {
+    const { deltaX, edge, axis } = onGestureMove(event)
+    if (deltaX === undefined || axis === 'y') {
       return
     }
     const raw = edge === 'left' ? Math.max(0, deltaX) : Math.min(0, deltaX)
