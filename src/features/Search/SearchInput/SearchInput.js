5b00370d3959c8a73c5d0c985f672baab0d3456a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-28 16:45:24 -0400

    Fix inputmode attribute to inputMode

--- src/features/Search/SearchInput/SearchInput.js
+++ src/features/Search/SearchInput/SearchInput.js
@@ -32,7 +32,7 @@ function SearchInput({ searchValue, error, setSearchValue, setError }) {
       spellCheck="true"
       autoFocus
       type="search"
-      inputmode="search"
+      inputMode="search"
       name="search"
       role="combobox"
       aria-autocomplete="list"
