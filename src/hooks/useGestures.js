90ce9c3d5f1be6ec58daa49969f4bb553d6e4c05
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-28 15:34:54 -0400

    Update useGestures.js

--- src/hooks/useGestures.js
+++ src/hooks/useGestures.js
@@ -15,11 +15,11 @@ function useGestures() { // thresholds
   } = useSwipe()
 
   const tapCounter = () => {
-    const doubleTapDelay = 300
+    const tapDelay = 300
     const now = performance.now()
     const deltaT = now - prevTap.current
-    const isSequence = deltaT < doubleTapDelay && deltaT > 0
-    tapCount.current = isSequence ? tapCount.current + 1 : 1 // 2. Update count: Increment if fast enough, otherwise reset to 1
+    const isSequence = deltaT < tapDelay && deltaT > 0
+    tapCount.current = isSequence ? tapCount.current + 1 : tapCount.current // 2. Update count: Increment if fast enough, otherwise reset to 1
     prevTap.current = now // 3. Update timestamp for next comparison
     return tapCount.current
   }
