6d5be12fbc6ff0e53cde5e53747acc69a719df2b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-30 12:44:36 -0400

    Update TextField.js

--- src/features/Profile/TextField/TextField.js
+++ src/features/Profile/TextField/TextField.js
@@ -1,25 +1,40 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import useDebounce from '../../../hooks/useDebounce'
 import EditButton from '../EditButton/EditButton'
-import SubmitButton from '../SubmitButton/SubmitButton'
+import CloseButton from '../CloseButton/CloseButton'
 import styles from './TextField.module.css'
 
 function TextField({ text }) {
   const [isOpen, setIsOpen] = useState(false)
+  const inputRef = useRef(null)
 
   const open = () => setIsOpen(true)
   const close = () => setIsOpen(false)
 
-  const onChange = useDebounce(event => {
-    // make request once finished typing
-    console.log('debounced onChange: ', event.target.value)
-  }, 500)
+  const update = () => {
+    
+    console.log('text: ', text)
+    console.log('inputRef.current.value: ', inputRef.current.value)
+    console.log('text === inputRef.current.value: ', text === inputRef.current.value)
+    
+    if (text === inputRef.current.value) {
+      return
+    }
+    // if (text === updateText) return
+    // else make request
+  }
+
+  useEffect(() => {
+    if (!isOpen) {
+      update()
+    }
+  }, [isOpen])
 
   return (
     <div className={styles.textField}>
       {isOpen 
         ?  <>
-             <input type="text" value={text} onChange={onChange} aria-label={`${text} input`} />
+             <input ref={inputRef} type="text" defaultValue={text} aria-label={`${text} input`} />
              <SubmitButton text={text} close={close} />
            </>
         : <>
