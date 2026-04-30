7f588e30e1aa1b1d2d29dd418a97fc4614df34d7
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-30 15:23:14 -0400

    Update Feed.js

--- src/features/Feed/Feed.js
+++ src/features/Feed/Feed.js
@@ -7,19 +7,19 @@ import Sentinel from './Sentinel/Sentinel'
 import styles from './Feed.module.css'
 
 function Feed() {
-  const [data, setData] = useState([1, 2, 3])
+  const [data, setData] = useState([
+    { video: 1, caption: 'test 1' },
+    { video: 2, caption: 'test 2' },
+    { video: 3, caption: 'test 3' }
+  ])
   
   return (
     <section className={styles.feed} role="feed">
-    
-      {data.map((item, index) => 
-        <FeedNode key={index} item={item} index={index + 1} count={data.length} />
-      )}
-{/*
-      <Sentinel onVisible={loadMoreData} />
-*/}
+      {data.map((item, index) => <FeedNode key={index} item={item} index={index + 1} count={data.length} />)}
+      
+      {/* <Sentinel onVisible={loadMoreData} /> */}
     </section>
   )
 }
 
-export default Feed // memo(Feed)
+export default Feed
