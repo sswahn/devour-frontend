2ce16d7d66473fbc34fe828817a5dc266b31a68b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-30 12:05:16 -0400

    Update TextField.js

--- src/features/Profile/TextField/TextField.js
+++ src/features/Profile/TextField/TextField.js
@@ -1,24 +1,30 @@
 import { useState } from 'react'
+import useDebounce from '../../../hooks/useDebounce'
 import EditButton from '../EditButton'
 import SubmitButton from '../SubmitButton'
 import styles from './TextField.module.css'
 
 function TextField({ text }) {
+  const debounce = useDebounce()
   const [isOpen, setIsOpen] = useState(false)
 
   const open = setIsOpen(true)
   const close = setIsOpen(false)
 
+  const onChange = debounce(event => {
+    // make request once finished typing
+  }, 500)
+
   return (
     <div className={styles.textField}>
       {isOpen 
         ?  <>
-             <input type="text" value={text} />
-             <SubmitButton close={close} />
+             <input type="text" value={text} onChange={onChange} aria-label={`${text} input`} />
+             <SubmitButton text={text} close={close} />
            </>
         : <>
             <span>{text}</span>
-            <EditButton open={open} />
+            <EditButton text={text} open={open} />
           </>
       }
     </div>
