c0ce35f977e4a552ac37ec38038b6bc4313c1445
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-30 11:51:59 -0400

    Update TextField.js

--- src/features/Profile/TextField/TextField.js
+++ src/features/Profile/TextField/TextField.js
@@ -1,9 +1,24 @@
+import { useState } from 'react'
 import styles from './TextField.module.css'
 
-function TextField() {
+function TextField({ text }) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const open = setIsOpen(true)
+  const close = setIsOpen(false)
 
   return (
     <div className={styles.textField}>
+     {isOpen 
+       ?  <>
+            <input type="text" value={text} />
+            <SubmitButton close={close} />
+          </>
+       : <>
+           <span>{text}</span>
+           <EditButton open={open} />
+         </>
+      }
     </div>
   )
 }
