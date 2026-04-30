34948b6ec9b80771ffb83a9aaad23e1b482b1d3d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-30 12:35:51 -0400

    Update and rename SubmitButton.js to CloseButton.js

---
+++ src/features/Profile/CloseButton/CloseButton.js
@@ -0,0 +1,29 @@
+import XmarkIcon from '../../../components/Icons/XmarkIcon/XmarkIcon'
+import styles from './CloseButton.module.css'
+
+function CloseButton({ text, close }) {
+ 
+  const action = () => {
+    close()
+  }
+
+  const onClick = event => {
+    navigation.vibrate?.()
+    action()
+  }
+  
+  const onKeyDown = event => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      action()
+    }
+  }
+
+  return (
+    <button className={styles.closeButton} onClick={onClick} onKeyDown={onKeyDown} type="button" aria-label={`submit new ${text}`}>
+      <XmarkIcon />
+    </button>
+  )
+}
+
+export default CloseButton
