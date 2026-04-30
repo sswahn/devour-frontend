a47460acd747b0f4ef053b87e1a005f21f0daede
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-30 10:11:30 -0400

    Update Notifications.js

--- src/features/Notifications/Notifications.js
+++ src/features/Notifications/Notifications.js
@@ -48,11 +48,7 @@ function Notifications() {
   
   const close = currentTarget => {
     const bottomSheet = currentTarget || bottomSheetRef.current
-    bottomSheet.addEventListener('transitionend', () => {
-      // bottomSheet.style.transition = ''
-      // bottomSheet.style.height = '0dvh'
-      action()
-    }, { once: true })
+    bottomSheet.addEventListener('transitionend', action, { once: true })
     bottomSheet.style.transform = 'translate3d(0, 100dvh, 0)'
   }
 
