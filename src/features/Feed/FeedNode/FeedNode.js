6306b6dc4b20e91690fe7e60867f7462768b3288
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-01 16:07:36 -0400

    Update FeedNode.js

--- src/features/Feed/FeedNode/FeedNode.js
+++ src/features/Feed/FeedNode/FeedNode.js
@@ -52,9 +52,7 @@ function FeedNode({ item, index, count }) {
       <TopNav />
 
       {/* item.videoUrl && <video ref={ref} src={item.videoUrl} preload="metadata" muted playsInline loop /> */}
-      {/* item.caption ?? */}
-      
-      <figcaption tabIndex="0">{item.caption}</figcaption>
+      {item.caption ?? <figcaption tabIndex="0">{item.caption}</figcaption>}
       
       <SideNav isDoubleTap={isDoubleTap} isLongPress={isLongPress} isPinch={isPinch} />
     </figure>
