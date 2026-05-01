35a640744aeda2598faff6e849037a91e0b6d1a0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-01 10:07:47 -0400

    Update RegistrationForm.js

--- src/features/Authentication/RegistrationForm/RegistrationForm.js
+++ src/features/Authentication/RegistrationForm/RegistrationForm.js
@@ -25,9 +25,9 @@ function RegistrationForm() {
   
   return (
     <form className={styles.registrationForm} onSubmit={onSubmit} aria-label="registration form">
-      <label htmlFor="username">Username:</label>
+      <label htmlFor="username">Username: <span aria-hidden="true">*</span></label>
       <input id="username" type="text" name="username" inputMode="username" autoComplete="username webauthn" required />
-      <label htmlFor="contact">Email or phone:</label>
+      <label htmlFor="contact">Email or phone: <span aria-hidden="true">*</span></label>
       <input id="contact" type="text" name="contact" inputMode="email" required /> {/* phone or email (needs validation) */}
       <button onClick={onClick} type="submit">Sign Up</button>
     </form>
