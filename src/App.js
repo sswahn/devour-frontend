fd2e28929d8d1d39166770ab8dba925fc4ad7691
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-01 15:36:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
-import useSelectionToSpeech from './hooks/useSelectionToSpeech'
 import ErrorBoundary from './ErrorBoundary'
 import Providers from './Providers'
 import Interface from './components/Interface/Interface'
 import './index.css'
 
 function App() {
-  useSelectionToSpeech()
   return (
     <StrictMode> 
       <ErrorBoundary> 
