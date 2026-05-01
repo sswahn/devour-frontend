9be582aa81511b85200a83c31ae491f610e3df25
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-01 09:28:50 -0400

    Update RegistrationForm.js

--- src/features/Authentication/RegistrationForm/RegistrationForm.js
+++ src/features/Authentication/RegistrationForm/RegistrationForm.js
@@ -2,19 +2,25 @@ import styles from './RegistrationForm.module.css'
 
 function RegistrationForm() {
 
+  const onClick = event => {
+    navigator.vibrate?.(50)
+  }
+
   const onSubmit = event => {
     event.preventDefault()
     const formData = new FormData(event.target)
     const request = {
-      username: formData.get('username')
+      username: formData.get('username'),
+      contact: formData.get('contact') // phone or email
     }
+    
   }
   
   return (
     <form className={styles.registrationForm} onSubmit={onSubmit} aria-label="registration form">
-      <input type="text" inputMode="username" />
-      <input type="text" /> {/* phone or email (needs validation) */}
-      <button type="submit">{/* <PaperPlane /> */} Submit</button>
+      <input type="text" name="username" inputMode="username" />
+      <input type="text" name="contact" /> {/* phone or email (needs validation) */}
+      <button onClick={onClick} type="submit">{/* <PaperPlane /> */} Submit</button>
     </form>
   )
 }
