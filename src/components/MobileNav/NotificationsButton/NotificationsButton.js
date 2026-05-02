2adf2ba74ea0d1fc9a14f4839d3fc6ccfd9c30f0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-02 12:36:42 -0400

    Update NotificationsButton.js

--- src/components/MobileNav/NotificationsButton/NotificationsButton.js
+++ src/components/MobileNav/NotificationsButton/NotificationsButton.js
@@ -1,5 +1,5 @@
 import { useRef } from 'react'
-import { overlays } from '../../../config'
+import { overlay } from '../../../config'
 import useOverlay from '../../../hooks/useOverlay'
 import BellIcon from '../../Icons/BellIcon/BellIcon'
 import styles from './NotificationsButton.module.css'
@@ -9,7 +9,7 @@ function NotificationsButton() {
   const { openOverlay } = useOverlay()
   
   const action = () => {
-    openOverlay(overlays.notifications, buttonRef.current)
+    openOverlay(overlay.notifications, buttonRef.current)
   }
   
   const onClick = event => {
