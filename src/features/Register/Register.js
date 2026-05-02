bca2c6a64b97cf2def254ffe50700c036416db5f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-02 12:07:26 -0400

    Enhance Register component with validation logic
    Refactor registration form with validation and error handling.

--- src/features/Register/Register.js
+++ src/features/Register/Register.js
@@ -1,14 +1,45 @@
+import { useState } from 'react'
 import styles from './Register.module.css'
 
 function Register() {
-
-  const onSubmit = event => {
-    
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(false)
+  
+  const onClick = event => {
+    navigator.vibrate?.(50)
   }
 
+  const validate = data => {
+    // validate username
+    // validate phone/email
+    // if invalid:
+    // break onSubmit with throw error
+  }
+
+  const onSubmit = event => {
+    try {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const request = {
+      username: formData.get('username'),
+      contact: formData.get('contact') // phone or email
+    }
+    validate(formData)
+
+    // navigator.credentials.create
+    } catch (error) {
+      setErrorMessage(error)
+    }
+  }
+  
   return (
-    <form className={styles.register} onSubmit={onSubmit}>
-    
+    <form className={styles.registrationForm} onSubmit={onSubmit} aria-label="registration form">
+      <label htmlFor="username">Username: <span aria-hidden="true">*</span></label>
+      <input id="username" type="text" name="username" inputMode="username" autoComplete="username webauthn" required />
+      <label htmlFor="contact">Email or phone: <span aria-hidden="true">*</span></label>
+      <input id="contact" type="text" name="contact" inputMode="email" required /> {/* phone or email (needs validation) */}
+      <button onClick={onClick} type="submit">Sign Up</button>
     </form>
   )
 }
