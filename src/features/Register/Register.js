4bfa30e390ab6341a8a8ec672524387141dcf1dd
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-02 12:00:00 -0400

    Create Register.js

---
+++ src/features/Register/Register.js
@@ -0,0 +1,16 @@
+import styles from './Register.module.css'
+
+function Register() {
+
+  const onSubmit = event => {
+    
+  }
+
+  return (
+    <form className={styles.register} onSubmit={onSubmit}>
+    
+    </form>
+  )
+}
+
+export default Register
