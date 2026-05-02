4f6fcb96696416e57fd889209875ab9a19207fb0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-02 09:47:22 -0400

    Update Profile.js

--- src/features/Profile/Profile.js
+++ src/features/Profile/Profile.js
@@ -27,7 +27,7 @@ function Profile() {
     biography: ''
    })
 
-  const updateProfile = property => setProfile(prev => {...prev, ...property })
+  const updateProfile = property => setProfile(prev => ({...prev, ...property }))
 
   const action = () => {
     closeOverlay()
