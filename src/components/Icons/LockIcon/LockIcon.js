f6a3c52ef0c0a7dd90633bfcdd4978b30d4c8875
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-03 08:52:16 -0400

    Change LockIcon to a function declaration

--- src/components/Icons/LockIcon/LockIcon.js
+++ src/components/Icons/LockIcon/LockIcon.js
@@ -1,4 +1,4 @@
-const LockIcon = ({ size = 24 }) => {
+function LockIcon({ size = 24 }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width={size} height={size} aria-hidden="true">
       {/* Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
