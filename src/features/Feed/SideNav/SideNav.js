6c93fd2554d21ff4be5f6857be98a436676ae418
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-03 10:51:18 -0400

    Refactor SideNav to remove isPinch prop
    Removed unused 'isPinch' prop from SideNav component.

--- src/features/Feed/SideNav/SideNav.js
+++ src/features/Feed/SideNav/SideNav.js
@@ -4,14 +4,14 @@ import ShareButton from '../ShareButton/ShareButton'
 import FullscreenButton from '../FullscreenButton/FullscreenButton'
 import styles from './SideNav.module.css'
 
-function SideNav({ isDoubleTap, isLongPress, isPinch }) {
+function SideNav({ isDoubleTap, isLongPress }) {
   
   return (
     <nav className={styles.sideNav} aria-label="video actions">
       <LikeButton isDoubleTap={isDoubleTap} />
       <CommentsButton />
       <ShareButton isLongPress={isLongPress} />
-      <FullscreenButton isPinch={isPinch} />
+      <FullscreenButton />
     </nav>
   )
 }
