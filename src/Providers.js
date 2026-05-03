7ccef51512b4af4d0933b53eaeee0d9bd8109441
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-03 15:08:14 -0400

    Add CommentsProvider to Providers component

--- src/Providers.js
+++ src/Providers.js
@@ -2,6 +2,7 @@ import { SessionProvider } from './components/Providers/SessionProvider'
 import { FocusTrapProvider } from './components/Providers/FocusTrapProvider'
 import { OverlayProvider } from './components/Providers/OverlayProvider'
 import { ProfileProvider } from './components/Providers/ProfileProvider'
+import { CommentsProvider } from './components/Providers/CommentsProvider'
 
 function Providers({ children }) {
   return (
@@ -9,7 +10,9 @@ function Providers({ children }) {
       <FocusTrapProvider>
         <OverlayProvider>
           <ProfileProvider>
-            {children}
+            <CommentsProvider>
+              {children}
+            </CommentsProvider>
           </ProfileProvider>
         </OverlayProvider>
       </FocusTrapProvider>
