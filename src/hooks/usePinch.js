6545a96163403ce0eca711b038af9457b0ae236a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-03 10:08:32 -0400

    Update usePinch.js

--- src/hooks/usePinch.js
+++ src/hooks/usePinch.js
@@ -9,9 +9,7 @@ function usePinch() {
   }
 
   const onPinchDown = event => {
-    const { pointerId, clientX, clientY, currentTarget } = event
-
-    currentTarget.setPointerCapture(pointerId)
+    const { clientX, clientY, pointerId } = event
 
     pointers.current.set(pointerId, { x: clientX, y: clientY })
 
@@ -19,33 +17,32 @@ function usePinch() {
   }
 
   const onPinchMove = event => {
-    if (!pointers.current.has(event.pointerId)) return {}
+    const { pointerId } = event
+    if (!pointers.current.has(pointerId)) {
+      return {}
+    }
 
-    pointers.current.set(event.pointerId, {
+    pointers.current.set(pointerId, {
       x: event.clientX,
       y: event.clientY
     })
 
-    if (pointers.current.size !== 2) return {}
-
+    if (pointers.current.size !== 2) {
+      return {}
+    }
+    
     const [p1, p2] = [...pointers.current.values()]
     const distance = getDistance(p1, p2)
-
-    let scale = 1
     let direction = 'none'
-
-    if (prevDistance.current != null) {
+    let scale = 1
+    
+    if (prevDistance.current !== null) {
       scale = distance / prevDistance.current
       direction = scale > 1 ? 'out' : 'in'
     }
-
     prevDistance.current = distance
 
-    return {
-      isPinching: true,
-      scale,
-      direction
-    }
+    return { isPinching: true, scale, direction }
   }
 
   const onPinchUp = event => {
@@ -72,64 +69,3 @@ function usePinch() {
 }
 
 export default usePinch
-
-/*
-import { useRef } from 'react'
-
-const usePinch = () => {
-  const prevDistance = useRef(-1)
-  const data = useRef([])
-
-  // The browser fires a new pointerdown event for every finger that touches the screen
-  const onPinchDown = event => {
-    const { clientX, clientY, pointerId } = event
-    const exists = data.current.find(p => p.id === pointerId) // 1. Prevent adding the same finger twice
-    if (!exists) {
-      data.current.push({ 
-        startX: clientX, 
-        startY: clientY,
-        id: pointerId
-      })
-    }
-    return { isPinching: data.current.length === 2 }
-  }
-
-  const onPinchMove = event => {
-    if (data.current.length !== 2) { 
-      return {}
-    }
-    const { clientX, clientY, pointerId } = event
-    const [ first, second ] = data.current
-    const isFirst = first.id === pointerId
-    const p1 = isFirst ? { clientX, clientY } : first
-    const p2 = isFirst ? second : { clientX, clientY }
-    const distance = Math.hypot(p2.clientX - p1.clientX, p2.clientY - p1.clientY)
-    let direction = 'none'
-    let ratio = 1
-    if (prevDistance.current > 0) {
-      ratio = distance / prevDistance.current
-      direction = ratio > 1 ? 'out' : 'in'
-    }  
-    prevDistance.current = distance
-    return { isPinching: data.current.length === 2, pinch: direction }
-  }
-
-  const onPinchUp = event => {
-    return { isPinching: data.current.length === 2 }
-  }
-
-  const onPinchCancel = event => {
-    data.current = []
-    prevDistance.current = -1
-  }
-
-  return {
-    onPinchDown,
-    onPinchMove,
-    onPinchUp,
-    onPinchCancel
-  }
-}
-
-export default usePinch
-*/
