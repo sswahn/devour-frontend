2176b9176b123095041878b46579ccebc707ddc0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-03 09:53:43 -0400

    Update FeedNode.js

--- src/features/Feed/FeedNode/FeedNode.js
+++ src/features/Feed/FeedNode/FeedNode.js
@@ -3,7 +3,7 @@ import { createObserver } from '../../../utilities/observer'
 import useGestures from '../../../hooks/useGestures'
 import TopNav from '../TopNav/TopNav'
 import SideNav from '../SideNav/SideNav'
-import Comments from '../Comments/Comments'
+import Comments from '../../Comments/Comments'
 import styles from './FeedNode.module.css'
 
 function FeedNode({ item, index, count }) {
