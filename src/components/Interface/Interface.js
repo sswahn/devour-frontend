51e1b7779e2f4071619094560fa38148629528ed
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-04 11:18:41 -0400

    Update Interface.js

--- src/components/Interface/Interface.js
+++ src/components/Interface/Interface.js
@@ -1,4 +1,4 @@
-import { useEffect, Suspense, lazy } from 'react'
+import { Suspense, lazy } from 'react'
 import useSelectionToSpeech from '../../hooks/useSelectionToSpeech'
 import Header from '../Header/Header'
 import Main from '../Main/Main'
@@ -8,36 +8,7 @@ const Overlays = lazy(() => import('../Overlays/Overlays'))
 
 function Interface() {
   useSelectionToSpeech()
-  // useContextMenu()
-  
-  // move this into a hook:
-  const onContextMenu = event => {
-    const { clientX, clientY, target } = event
-    console.log('contextmenu event fired.')
-    
-    // leave preventDefualt commented-out while developing:
-    // event.preventDefault()
-
-    // To get the render location
-    // Check if triggered by keyboard (coordinates will be 0 or -1)
-    /* Example: 
-      let x = clientX
-      let y = clientY
-      if (x <= 0 && y <= 0) { // means keyboard initiated
-        const rect = target.getBoundingClientRect()
-        x = rect.left
-        y = rect.bottom
-      }
-      renderMenu(x, y)
-    */
-  }
-
-  useEffect(() => {
-    document.addEventListener('contextmenu', onContextMenu)
-    return () => {
-      document.removeEventListener('contextmenu', onContextMenu)
-    }
-  }, [])
+  useContextMenu()
   
   return (
     <>
