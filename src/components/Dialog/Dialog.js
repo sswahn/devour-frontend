e55421ee4b78f93724ba49a856bf68a76e991052
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-04 08:16:08 -0400

    Update Dialog component props and remove state
    Refactor Dialog component to accept isOpen and close props.

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,27 +1,6 @@
-import { useState, useEffect } from 'react'
 import styles from './Dialog.module.css'
 
-function Dialog({ dialogRef, onClose, children }) {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const open = () => {
-    if (!dialogRef.current.open) {
-      setIsOpen(true)
-    }
-  }
-  
-  const close = () => {
-    setIsOpen(false)
-    onClose()
-  }
-
-  useEffect(() => {
-    if (dialogRef.current) {
-      open()
-    }
-  }, [])
- 
-  
+function Dialog({ dialogRef, close, isOpen, children }) { 
   return (
     <dialog id="dialog" ref={dialogRef} className={styles.dialog} hidden={!isOpen} onClose={close}>
       {children}
