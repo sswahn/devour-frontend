d653cd88e831b965b147343628e5af220ec9cae0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-04 11:16:14 -0400

    Update Interface.js

--- src/components/Interface/Interface.js
+++ src/components/Interface/Interface.js
@@ -8,12 +8,28 @@ const Overlays = lazy(() => import('../Overlays/Overlays'))
 
 function Interface() {
   useSelectionToSpeech()
-
+  // useContextMenu()
+  
+  // move this into a hook:
   const onContextMenu = event => {
-    console.log('contextmenu event fired, and prevented.')
-    // leave commented while developing:
+    const { clientX, clientY, target } = event
+    console.log('contextmenu event fired.')
+    
+    // leave preventDefualt commented-out while developing:
     // event.preventDefault()
-    // render <ContextMenu />
+
+    // To get the render location
+    // Check if triggered by keyboard (coordinates will be 0 or -1)
+    /* Example: 
+      let x = clientX
+      let y = clientY
+      if (x <= 0 && y <= 0) { // means keyboard initiated
+        const rect = target.getBoundingClientRect()
+        x = rect.left
+        y = rect.bottom
+      }
+      renderMenu(x, y)
+    */
   }
 
   useEffect(() => {
