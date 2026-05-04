6f93c7b1290d883783dc2a6c5935506b0d28b5ac
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-04 13:02:46 -0400

    Fix label attributes from 'forHtml' to 'htmlFor'

--- src/features/Profile/ImageEditor/ImageEditor.js
+++ src/features/Profile/ImageEditor/ImageEditor.js
@@ -54,19 +54,19 @@ function ImageEditor() {
         <canvas ref={canvasRef} style={{ maxWidth: '100%', height: 'auto' }} />
       </div>
       <div>
-        <label forHtml="crop">Crop:</label>
+        <label htmlFor="crop">Crop:</label>
         <input id="crop" type="range" name="crop" min="0" max="100" value="50" styles={{ writingMode: 'vertical-lr' }}>
       </div>
        <div>
-        <label forHtml="rotate">Rotate:</label>
+        <label htmlFor="rotate">Rotate:</label>
         <input id="rotate" type="range" name="rotate" min="0" max="100" value="50" styles={{ writingMode: 'vertical-lr' }}>
       </div>
       <div>
-        <label forHtml="zoom">Zoom:</label>
+        <label htmlFor="zoom">Zoom:</label>
         <input id="zoom" type="range" name="zoom" min="0" max="100" value="50" styles={{ writingMode: 'vertical-lr' }}>
       </div>
       <form onSubmit={onSubmit}>
-        <label forHtml="upload">Select an image:</label>
+        <label htmlFor="upload">Select an image:</label>
         <input id="upload" type="file"  name="upload" accept="image/*">
         <button type="submit">Upload Image</button>
       </form>
