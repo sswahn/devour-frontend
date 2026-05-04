71ec43b517c2e1751374666e7d7f2bb0949d2a93
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-04 08:35:21 -0400

    Update DialogProvider.js

--- src/components/Providers/DialogProvider.js
+++ src/components/Providers/DialogProvider.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useCallback, createContext, useContext } from 'react'
+import { useState, useRef, useEffect, useCallback, createContext } from 'react'
 
 const DialogContext = createContext(null)
 
