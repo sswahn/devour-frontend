62b174cb3db3717c06610f4d9757804812887295
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-04 10:40:49 -0400

    Update ContextMenu.js

--- src/components/ContextMenu/ContextMenu.js
+++ src/components/ContextMenu/ContextMenu.js
@@ -1,9 +1,18 @@
+import styles from './Menu.module.css'
 
-function ContextMenu() {
+function ContextMenu({ items }) {
 
   // global element that spawns at pointer location, yet doesnt go off screen.
+  // when a specific element is targeted, if it is in the list of elements with instructions
+  // add the info icon and information text to open a dialog element
+  // also some elements have specific actions, such as feed node: Like, Share, Flag, etc.
+  // perhaps use a config for ContextMenu, that uses "info", "actions" and contains keys to the elements,
+  // and related content
+  
   return (
-    <div></div>
+    <menu className={styles.contextMenu}>
+     {items.map(item => <li>{item.text}</li>)}
+    </menu>
   )
 }
 
