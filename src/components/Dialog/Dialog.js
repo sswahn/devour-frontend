31e61790cefee19d3914489d9335f283ef1842aa
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-04 07:52:46 -0400

    Update Dialog.js

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -5,8 +5,13 @@ function Dialog() {
   const [isOpen, setIsOpen] = useState(false)
   const dialogRef = useRef(null)
 
-  const open = () => {}
-  const close = () => {}
+  const open = () => {
+    dialogRef.current.showModal()
+  }
+  
+  const close = () => {
+    dialogRef.current.close()
+  }
   
   // there will only be one, and children will be injected.
   // possibly via DialogContext
