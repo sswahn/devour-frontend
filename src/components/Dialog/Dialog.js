5f11963e0e2b4f475a7dc5d9ba029497bc9498da
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-04 07:51:55 -0400

    Update Dialog.js

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import styles from './Dialog.module.css'
 
 function Dialog() {
   const [isOpen, setIsOpen] = useState(false)
+  const dialogRef = useRef(null)
+
+  const open = () => {}
+  const close = () => {}
   
   // there will only be one, and children will be injected.
   // possibly via DialogContext
   
   return (
-    <dialog className={styles.dialog} hidden={!isOpen}>
+    <dialog id="dialog" ref={dialogRef} className={styles.dialog} hidden={!isOpen}>
       
     </dialog>
   )
