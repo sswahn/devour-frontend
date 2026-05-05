5abfc200b9da38ace2394f9cb09ad899367b5107
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-05 07:11:01 -0400

    Update LoginForm.js

--- src/features/Login/LoginForm/LoginForm.js
+++ src/features/Login/LoginForm/LoginForm.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { api } from '../../../config'
 import useSession from '../../../hooks/useSession'
 import Input from '../../../components/Input/Input'
 import styles from './LoginForm.module.css'
@@ -9,11 +10,13 @@ function LoginForm() {
   const [data, setData] = useState([])
   const [errors, setErrors] = useState({ username: '' })
 
+  /*
   const onChange = event => {
     if (errors.username) {
       setErrors({ username: '' })
     }
   }
+  */
   
   const validateUsername = username => {
     if (!username.trim()) {
@@ -28,21 +31,25 @@ function LoginForm() {
     }
   }
   
-  const onSubmit = event => {
+  const onSubmit = async event => {
     try {
       event.preventDefault()
       navigator.vibrate?.(50)
       setLoading(true)
       const formData = new FormData(event.target)
-      const request = {
-        username: validateUsername(formData.get('username'))
-      }
-        
+
       // initiate auth call to backend
       // response returns challenge
       // challenge is signed by browser/device:
       // (call navigator.credentials.get() for signature)
       // send signature to backend for verification and tokens
+      
+      // const credentials = await navigator.credentials.get()
+      const request = {
+        username: validateUsername(formData.get('username')),
+        credentials: ''
+      }
+      // const response = await server.post(api.login, request)
       
       // setSession({ isAuthenticated: true, ...response.message.userData})
     } catch (error) {
@@ -51,7 +58,7 @@ function LoginForm() {
       setLoading(false)
     }
   }
-
+/*
   const onInvalid = event => {
     event.preventDefault()
     setErrors(prev => ({ ...prev, [event.target.name]: event.target.validationMessage }))
@@ -63,12 +70,15 @@ function LoginForm() {
       document.removeEventListener('invalid', onInvalid, true)
     }
   }, [])
+  
 
   useEffect(() => {
     if (errors.username) {
       // need to place focus on first invalid input
     }
   }, [errors.username])
+
+  */
   
   return (
     <form className={styles.loginForm} onSubmit={onSubmit} aria-label="login form">
@@ -88,7 +98,6 @@ function LoginForm() {
         className={errors.username ? styles.invalid : ''}
         name="username" 
         type="text"
-        onChange={onChange}
         inputMode="email" 
         autoComplete="username webauthn" 
         aria-invalid={errors.username ? true : undefined} 
