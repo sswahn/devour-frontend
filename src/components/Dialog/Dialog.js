178a7816aad49157344e6043cbe6db84740dc7bc
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-05 14:27:22 -0400

    Update Dialog.js

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,8 +1,8 @@
 import styles from './Dialog.module.css'
 
-function Dialog({ dialogRef, close, content }) { 
+function Dialog({ dialogRef, content }) { 
   return (
-    <dialog id="dialog" ref={dialogRef} className={styles.dialog} onClose={close}>
+    <dialog id="dialog" ref={dialogRef} className={styles.dialog}>
       {content}
     </dialog>
   )
