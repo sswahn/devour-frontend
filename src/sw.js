a5411d297f50b5151b5a99b69c23972df23344ee
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-05 10:31:25 -0400

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,16 @@
 
+const cacheResponse = (request, response) => {
+  if (response.ok) {
+    const cache = await caches.open('getRequests')
+    cache.put(request, response.clone())
+  }
+}
 
 const fetchRequest = async event => {
     try {
     const { request } = event
     const response = await fetch(request)
-    const cache = await caches.open('getRequests')
-    cache.put(request, response.clone())
+    cahceResponse(request, response)
     return response
   } catch (error) {
     return caches.match(request)
