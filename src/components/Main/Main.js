7d3c4813ef43c629148f2fd27b0955b3275a6d6d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-06 17:08:30 -0400

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,15 @@
-import { useState, useEffect, Suspense, lazy } from 'react'
+import { Suspense, lazy } from 'react'
+import useOverlay from '../../hooks/useOverlay'
 import Suggestions from '../Suggestions/Suggestions'
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
 const Feed = lazy(() => import('../../features/Feed/Feed'))
 import styles from './Main.module.css'
 
 function Main() {
+  const { isActive } = useOverlay()
   
   return (
-    <main className={styles.main} aria-description="When text is highlighted, it will automatically be read aloud.">
+    <main className={styles.main} inert={!!isActive} aria-description="When text is highlighted, it will automatically be read aloud.">
     
       {/* <Suggestions /> etc. */}
 
