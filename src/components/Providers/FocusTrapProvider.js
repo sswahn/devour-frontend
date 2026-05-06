57ec52d5f1a5e376e8499f8c9a7d54b56c03a4cb
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-06 18:14:25 -0400

    Update FocusTrapProvider.js

--- src/components/Providers/FocusTrapProvider.js
+++ src/components/Providers/FocusTrapProvider.js
@@ -1,46 +1,40 @@
-import { useState, useRef, useCallback, createContext } from 'react'
+import { useState, useRef, useEffect, createContext } from 'react'
+import useOverlay from '../../hooks/useOverlay'
 
 const FocusTrapContext = createContext(null)
 
 const selector = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
 
 function FocusTrapProvider({ children }) {
+  const { isActive } = useOverlay()
   const [isMounted, setIsMounted] = useState(false)
-  const overlayRef = useRef(null)
-  
-  const focusRef = useCallback(node => {
-    if (node !== null) {
-      overlayRef.current = node
-      overlayRef.current.focus()
-      setIsMounted(true)
-      return () => {
-        overlayRef.current = null
-        setIsMounted(false)
-      }
-    }
-  }, [])
+  const focusTrapRef = useRef(null)
 
   const focusLast = event => {
-    const elements = overlayRef.current.querySelectorAll(selector)
-
-    console.log('focus (last) element: ', elements[elements.length - 1])
-    
+    const elements = focusTrapRef.current.querySelectorAll(selector)
     elements[elements.length - 1]?.focus()
   }
   
   const focusFirst = event => {
-    const elements = overlayRef.current.querySelectorAll(selector)
-
-    console.log('focus (first) element: ', elements[0])
-    
+    const elements = focusTrapRef.current.querySelectorAll(selector)    
     elements[0]?.focus()
   }
+
+  useEffect(() => {
+    if (isActive) {
+      setIsMounted(true)
+    }
+  }, [isActive])
     
   return (
     <FocusTrapContext.Provider value={{ overlayRef, focusRef }}>
-      {isMounted && <div onFocus={focusLast} tabIndex={0}></div>}
-        {children}
-      {isMounted && <div onFocus={focusFirst} tabIndex={0}></div>}
+      {isMounted &&  (
+        <div ref={focusTrapRef}>
+          <div onFocus={focusLast} tabIndex={0}></div>}
+            {children}
+          <div onFocus={focusFirst} tabIndex={0}></div>}
+        </div>
+      )}
     </FocusTrapContext.Provider>
   )
 }
