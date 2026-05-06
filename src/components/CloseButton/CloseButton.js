b9ec6886cefbee6c2d3c452334a6eb1f652411a7
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-06 13:04:34 -0400

    Update CloseButton.js

--- src/components/CloseButton/CloseButton.js
+++ src/components/CloseButton/CloseButton.js
@@ -3,27 +3,15 @@ import styles from './CloseButton.module.css'
 
 function CloseButton({ overlay, close }) {
 
-  const action = () => {
-    close()
-  }
-  
   const onClick = event => {
     navigator.vibrate?.(50)
-    action()
-  }
-
-  const onKeyDown = event => {
-    if (event.key === 'Enter') {
-      event.preventDefault()
-      action()
-    }
+    close()
   }
  
  return (
    <button 
      className={styles.closeButton} 
      onClick={onClick} 
-     onKeyDown={onKeyDown}
      type="button" 
      aria-label={`close ${overlay}`}>
      <ArrowLeftIcon />
