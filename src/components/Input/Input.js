68a159f04c5066c731b225a161fde28ffb4d3cbc
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-06 07:42:45 -0400

    Update Input.js

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -27,7 +27,7 @@ const Input = ({
     
     console.log('should be the form: ', form)
     console.log('form elements: ', form.elements)
-    console.log('first element in form [0]: ', form.elemnts[0])
+    console.log('first element in form [0]: ', form.elements[0])
     
     if (input?.closest('form').elements[0] === input) {
       input.focus()
