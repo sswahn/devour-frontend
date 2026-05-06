e81cafc54d6c9a192963dbed1d491ed825d0855d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-06 07:46:15 -0400

    Update Input.js

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -58,7 +58,7 @@ const Input = ({
         autoComplete={autoComplete}
         aria-invalid={errorMessage ? true : undefined} 
         aria-errormessage={errorMessage ? `error-${id}` : undefined}
-        required={required}
+     //   required={required}
         {...props} />
       {errorMessage && <p id={`error-${id}`} role="alert">{errorMessage}</p>}
     </div>
