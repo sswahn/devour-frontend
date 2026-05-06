f3522511afe5fc064108db2df1451060209db5b0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-06 11:19:16 -0400

    Update HomeButton.js

--- src/components/MobileNav/HomeButton/HomeButton.js
+++ src/components/MobileNav/HomeButton/HomeButton.js
@@ -2,31 +2,19 @@ import HomeIcon from '../../Icons/HomeIcon/HomeIcon'
 import styles from './HomeButton.module.css'
 
 function HomeButton() {
-
-  const action = () => {
+  
+  const onClick = event => {
+    navigator.vibrate?.(50)
     window.scrollTo({
       behavior: 'smooth',
       top: 0
     })
   }
   
-  const onClick = event => {
-    navigator.vibrate?.(50)
-    action()
-  }
-
-  const onKeyDown = event => {
-    if (event.key === 'Enter') {
-      event.preventDefault()
-      action()
-    }
-  }
-  
   return (
     <button 
       className={styles.homeButton} 
       onClick={onClick} 
-     // onKeyDown={onKeyDown} 
       type="button" 
       aria-label="scroll to top">
       <HomeIcon />  
