7b0616def53ee72b8bba7145c4419bd6cf0023d9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-06 18:32:54 -0400

    Update FocusTrap.js

--- src/components/FocusTrap/FocusTrap.js
+++ src/components/FocusTrap/FocusTrap.js
@@ -1,1 +1,28 @@
-//
+import { useState, useRef, useEffect } from 'react'
+import useOverlay from '../../hooks/useOverlay'
+const selector = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+
+function FocusTrap({ children }) {
+  const [isMounted, setIsMounted] = useState(false)
+  const focusTrapRef = useRef(null)
+
+  const focusLast = event => {
+    const elements = focusTrapRef.current.querySelectorAll(selector)
+    elements[elements.length - 2]?.focus()
+  }
+  
+  const focusFirst = event => {
+    const elements = focusTrapRef.current.querySelectorAll(selector)
+    elements[1]?.focus()
+  }
+    
+  return (
+    <div ref={focusTrapRef}>
+      <div onFocus={focusLast} tabIndex={0}></div>
+        {children}
+      <div onFocus={focusFirst} tabIndex={0}></div>
+    </div>
+  )
+}
+
+export default FocusTrap
