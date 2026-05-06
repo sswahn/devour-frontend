b2a3b158bd8b595c7aa8f16d18ebd213e57b9e11
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-06 18:30:06 -0400

    Refactor Providers component to simplify nesting

--- src/Providers.js
+++ src/Providers.js
@@ -1,5 +1,4 @@
 import { SessionProvider } from './components/Providers/SessionProvider'
-import { FocusTrapProvider } from './components/Providers/FocusTrapProvider'
 import { OverlayProvider } from './components/Providers/OverlayProvider'
 import { ProfileProvider } from './components/Providers/ProfileProvider'
 import { DialogProvider } from './components/Providers/DialogProvider'
@@ -8,17 +7,15 @@ import { ContentProvider } from './components/Providers/ContentProvider'
 function Providers({ children }) {
   return (
     <SessionProvider>
-      <FocusTrapProvider>
-        <OverlayProvider>
-          <ProfileProvider>
-            <DialogProvider>
-              <ContentProvider>
-                {children}
-              </ContentProvider>
-            </DialogProvider>
-          </ProfileProvider>
-        </OverlayProvider>
-      </FocusTrapProvider>
+      <OverlayProvider>
+        <ProfileProvider>
+          <DialogProvider>
+            <ContentProvider>
+              {children}
+            </ContentProvider>
+          </DialogProvider>
+        </ProfileProvider>
+      </OverlayProvider>
     </SessionProvider>
   )
 }
