b10006a1456c40d35ec44e0f98c576d5927bee54
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-06 17:26:03 -0400

    Update FocusTrapProvider.js

--- src/components/Providers/FocusTrapProvider.js
+++ src/components/Providers/FocusTrapProvider.js
@@ -33,7 +33,7 @@ function FocusTrapProvider({ children }) {
 
     console.log('focus (first) element: ', elements[0])
     
-    elements[0]?.focus({ preventScroll: true })
+    elements[0]?.focus()
   }
     
   return (
