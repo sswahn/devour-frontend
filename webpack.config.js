0327a5897e72f1f754a2b3d9724858539783bbd2
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-06 14:10:40 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,80 @@
-const { ProvidePlugin } = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    
-  mode: 'development',
-  
-  entry: './src/index.js',
-    
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(process.cwd(), 'build'),
-    publicPath: '/devour-frontend/',
-    clean: true
-  },
-    
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"]
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  },
-    
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(process.cwd(), 'public', 'index.html'),
-    }),
-    new MiniCssExtractPlugin(),
-    new ProvidePlugin({ React: 'react' })
-  ],
-    
-  resolve: {
-    extensions: ['.js', '.jsx'] // ['.js', '.jsx', '.mjs', '.*'],
-  },
+module.exports = (env, argv) => {
+  const isDev = argv.mode === 'development'
 
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
+  return {
+    mode: isDev ? 'development' : 'production',
+
+    entry: './src/index.js',
+
+    output: {
+      filename: isDev ? '[name].js' : '[name].[contenthash].js',
+      path: path.resolve(process.cwd(), 'build'),
+      publicPath: '/devour-frontend/',
+      clean: true
     },
-    runtimeChunk: 'single'
-  },
-    
-  devServer: {
-    historyApiFallback: true,
-    allowedHosts: 'all',
-  },
-    
-  stats: {
-    errorDetails: true,
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: 'defaults' }],
+                ['@babel/preset-react', { runtime: 'automatic' }]
+              ],
+              cacheDirectory: true
+            }
+          }
+        },
+
+        {
+          test: /\.css$/i,
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader'
+          ]
+        }
+      ]
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(process.cwd(), 'public', 'index.html')
+      }),
+
+      !isDev && new MiniCssExtractPlugin()
+    ].filter(Boolean),
+
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      },
+      runtimeChunk: 'single'
+    },
+
+    devServer: {
+      static: {
+        directory: path.join(process.cwd(), 'build')
+      },
+      historyApiFallback: true,
+      allowedHosts: 'all',
+      hot: true,
+      open: true
+    },
+
+    stats: {
+      errorDetails: true
+    }
   }
 }
