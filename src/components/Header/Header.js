84e813d77cdac3df18cc2098b73801d5a0978059
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-07 07:32:34 -0400

    Add inert attribute to header based on overlay state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef  } from 'react'
 import useScrollEffect from '../../hooks/useScrollEffect'
 import useSession from '../../hooks/useSession'
+import useOverlay from '../../hooks/useOverlay'
 import HomeIcon from '../Icons/HomeIcon/HomeIcon'
 import DashboardButton from './DashboardButton/DashboardButton'
 import LoginButton from './LoginButton/LoginButton'
@@ -11,6 +12,7 @@ import Avatar from '../Avatar/Avatar'
 const Header = ({ openAuthentication, openDashboard }) => {
   const { session } = useSession()
   const { scrollEffect } = useScrollEffect()
+  const { isActive } = useOverlay()
   const headerRef = useRef(null)
 
   useEffect(() => {
@@ -18,7 +20,7 @@ const Header = ({ openAuthentication, openDashboard }) => {
   }, [])
 
   return (
-    <header ref={headerRef} className={styles.header}>
+    <header ref={headerRef} className={styles.header} inert={!!isActive}>
       <div>
         <button onClick={() => navigator.vibrate(50)} type="button" aria-label="home">
           <HomeIcon />
