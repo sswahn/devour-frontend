21874478ba3967d4d3b9722e9b055868289a7449
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-07 15:04:02 -0400

    Update Notifications.js

--- src/features/Notifications/Notifications.js
+++ src/features/Notifications/Notifications.js
@@ -95,8 +95,8 @@ function Notifications() {
     }
     const threshold = window.innerHeight * 0.25
     const movement = Math.abs(deltaY)
-    currentTarget.style.transition = ''
-    currentTarget.style.transform = ''
+    currentTarget.style.removeProperty('transition')
+    currentTarget.style.removeProperty('transform')
     if (direction === 'up' && movement > threshold) {
       setState('expand')
     } else if (direction === 'down' && movement > 10) {
