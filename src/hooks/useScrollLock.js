af4c48f057225ab95ecbaf0dfbb7cbed0cf69103
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-08 09:02:27 -0400

    Create useScrollLock.js

---
+++ src/hooks/useScrollLock.js
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+
+function useScrollLock({ isActive }) {
+  
+  const lockScroll = () => {
+    const { scrollY } = window
+    const html = document.documentElement
+    html.classList.add('lockScroll')
+    return () => {
+      html.removeAttribute('class')
+      window.scrollTo(0, scrollY)
+    }
+  }
+  
+  useEffect(() => {
+    if (!isActive) {
+      return
+    }
+    const unlockScroll = lockScroll()
+    return () => {
+      unlockScroll()
+    }
+  }, [isActive])
+
+}
+
+export default useScrollLock
