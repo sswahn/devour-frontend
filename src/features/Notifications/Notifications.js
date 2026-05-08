0e93b2340999f4dd6efefdf034eb992edd58625f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-08 12:11:08 -0400

    Update Notifications.js

--- src/features/Notifications/Notifications.js
+++ src/features/Notifications/Notifications.js
@@ -92,7 +92,7 @@ function Notifications() {
 
   const applyResistance = deltaY => {
     const limit = 150
-    const k = 600 // increase to make growth feel heavier
+    const k = state === 'expand' && deltaY < 0 ? 600 : 400 // increase to make growth feel heavier
     const absDeltaY = Math.abs(deltaY)
     const translation = limit * (absDeltaY / (absDeltaY + k))
     return Math.sign(deltaY) * translation
