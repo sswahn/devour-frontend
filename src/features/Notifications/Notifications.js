01dbaf834dae6f7c87975a0b96915c3647ee400b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-08 09:47:18 -0400

    Refactor resistance calculation in Notifications.js
    Refactor resistance calculation for better clarity.

--- src/features/Notifications/Notifications.js
+++ src/features/Notifications/Notifications.js
@@ -97,7 +97,7 @@ function Notifications() {
       return
     }
     const resistance = state === 'expand' && deltaY < 0 
-      ? (Math.abs(deltaY) * 10) / (Math.abs(deltaY) + 20) // 10 + Math.abs(deltaY) * 0.5 
+      ? 10 //(Math.abs(deltaY) * 10) / (Math.abs(deltaY) + 20) // 10 + Math.abs(deltaY) * 0.5 
       : 200
     const translateY = deltaY / (1 + Math.abs(deltaY) / resistance)
     throttleTransition(translateY, currentTarget)
