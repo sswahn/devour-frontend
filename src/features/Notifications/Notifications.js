b4292d25694e6fd809ee949419a67b7f644c5b37
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-08 07:02:59 -0400

    Adjust resistance calculation in Notifications.js

--- src/features/Notifications/Notifications.js
+++ src/features/Notifications/Notifications.js
@@ -96,7 +96,7 @@ function Notifications() {
     if (deltaY === undefined || axis === 'x') {
       return
     }
-    const resistance = state === 'expand' && deltaY < 0 ? 20 + Math.abs(deltaY) * 0.5 : 200
+    const resistance = state === 'expand' && deltaY < 0 ? 10 + Math.abs(deltaY) * 0.5 : 200
     const translateY = deltaY / (1 + Math.abs(deltaY) / resistance) // asymptotic
     throttleTransition(translateY, currentTarget)
   }
