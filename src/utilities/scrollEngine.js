3ecf4f68442b13328765e66feeba935856210a1a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-09 18:28:10 -0400

    Update scrollEngine.js

--- src/utilities/scrollEngine.js
+++ src/utilities/scrollEngine.js
@@ -17,7 +17,7 @@ function notify(data) {
 }
 
 function update(timestamp) {
-  const scrollY = element.scrollY
+  const scrollY = element.scrollTop
   
   // Calculate change in Y
   deltaY = scrollY - scrollStart 
@@ -59,7 +59,7 @@ function onScroll(event) {
 }
 
 function onScrollEnd(event) {
-  scrollStart = element.scrollY
+  scrollStart = element.scrollTop
   
   notify({
     deltaY,
