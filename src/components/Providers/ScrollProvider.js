53f8101e00a5ae34268f9251636cd6d431f61159
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-09 16:09:58 -0400

    Update ScrollProvider.js

--- src/components/Providers/ScrollProvider.js
+++ src/components/Providers/ScrollProvider.js
@@ -1,17 +1,27 @@
-import { useRef, useCallback, createContext } from 'react'
+import { useState, useRef, useCallback, useEffect, createContext } from 'react'
+import scroll from '../utilities/scrollEngine'
 
 const ScrollContext = createContext(null)
 
 function ScrollProvider({ children }) {
+  const [isSet, setIsSet] = useState(false)
   const getScrollRef = useRef(null)
+  
   const setScrollRef = useCallback(node => {
     if (node) {
       getScrollRef.current = node  
+      setIsSet(true)
     }
     return () => {
       getScrollRef.current = null
     }
   }, [])
+
+  useEffect(() => {
+    if (isSet) {
+      scroll.get(getScrollRef.current)
+    }
+  }, [isSet])
   
   return (
     <ScrollContext.Provider value={{ getScrollRef, setScrollRef }}>
