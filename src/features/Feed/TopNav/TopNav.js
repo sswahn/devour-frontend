a6fa8619cceca3414e088ad466f890ba406044d9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-09 08:26:22 -0400

    Update TopNav.js

--- src/features/Feed/TopNav/TopNav.js
+++ src/features/Feed/TopNav/TopNav.js
@@ -3,9 +3,11 @@ import Dropdown from '../../../components/Dropdown/Dropdown'
 import styles from './TopNav.module.css'
 
 function TopNav() {
+  const isFullscreen = document.fullscreenElement
+  
   return (
-    <nav className={`topNav ${styles.topNav}`} aria-label="top menu">
-      {/* isFullscreen && <CloseButton overlay="feed overlay" close={closeFeed} /> */}
+    <nav className={`topNav ${styles.topNav} ${isFullscreen ? styles.fullscreen : ''}`} aria-label="top menu">
+      {isFullscreen && <CloseButton overlay="feed overlay" close={closeFeed} />}
       <Dropdown items={[1,2,3]} />
     </nav>
   )
