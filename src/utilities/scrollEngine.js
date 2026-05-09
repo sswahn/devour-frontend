056e20f166e030d5ae4394c32aad65a5d5cf5af4
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-09 19:00:30 -0400

    Update scrollEngine.js

--- src/utilities/scrollEngine.js
+++ src/utilities/scrollEngine.js
@@ -38,6 +38,8 @@ function update(timestamp) {
   // Formula: (currentRawVelocity * smoothingFactor) + (PreviousSmoothedVelocity * (1 - Factor))
   // (smoothingFactor: 0 < factor <= 1. Smaller = smoother.
   velocity = (rawVelocity * 0.05) + (velocity * (1 - 0.05))
+  // try for a buttery scroll:
+  //velocity = (rawVelocity * 0.03) + (velocity * 0.97)
 
   console.log('velocity: ', velocity)
   
