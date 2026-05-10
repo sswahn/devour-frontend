2975d0fe877c64a9e22d43ca3ad711721592bf70
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-10 12:35:34 -0400

    Update config.js

--- src/features/Profile/config.js
+++ src/features/Profile/config.js
@@ -1,2 +1,11 @@
+import SearchIcon from '../../components/Icons/SearchIcon/SearchIcon'
+import MessageIcon from '../../components/Icons/MessageIcon/MessageIcon'
+import FlagIcon from '../../components/Icons/FlagIcon/FlagIcon'
+import UserXmarkIcon from '../../components/Icons/UserXmarkIcon/UserXmarkIcon'
 
-export const dropdown = []
+export const dropdown = [
+  { icon: <SearchIcon />, text: 'Search profile', method: () => alert('Opened search.') },
+  { icon: <MessageIcon />, text: 'Send message', method: () => alert('DM opened.') },
+  { icon: <FlagIcon />, text: 'Flag profile', method: () => alert('Profile reported.') },
+  { icon: <UserXmarkIcon />, text: 'Block user', method: () => alert('User blocked.') }
+]
