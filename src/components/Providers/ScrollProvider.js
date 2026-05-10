590657a405e552351666f0bc7063d811a1b9c566
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-10 07:25:52 -0400

    Update ScrollProvider.js

--- src/components/Providers/ScrollProvider.js
+++ src/components/Providers/ScrollProvider.js
@@ -3,8 +3,9 @@ import scroll from '../../utilities/scrollEngine'
 
 const ScrollContext = createContext(null)
 
-function ScrollProvider({ children }) {
-  const getScrollRef = useRef(null)
+const ScrollProvider = ({ children }) => {
+  const ticking = useRef(0)
+  const ScrollRef = useRef(null)
   const setScrollRef = useCallback(node => {
     if (node) {
       getScrollRef.current = node  
@@ -12,6 +13,65 @@ function ScrollProvider({ children }) {
     }
     return () => {
       getScrollRef.current = null
+    }
+  }, [])
+
+  const update = timestamp => {
+    const scrollY = element.scrollTop
+
+    // Calculate change in Y
+    deltaY = scrollY - scrollStart 
+    
+    // Calculate current scroll direction
+    const dY = scrollY - prevScrollY
+    direction = dY > 0 ? 'down' : dY < 0 ? 'up' : 'idle'
+    
+    // Set prevScrollY for use in next frame
+    prevScrollY = scrollY
+  
+    // Calculate scroll velocity
+    const deltaTime = timestamp - prevTimestamp
+    const rawVelocity = dY / deltaTime
+  
+    // Formula: (currentRawVelocity * smoothingFactor) + (PreviousSmoothedVelocity * (1 - Factor))
+    // (smoothingFactor: 0 < factor <= 1. Smaller = smoother.
+    velocity = (rawVelocity * 0.05) + (velocity * (1 - 0.05))
+    // try for a buttery scroll:
+    //velocity = (rawVelocity * 0.03) + (velocity * 0.97)
+  
+    console.log('velocity: ', velocity)
+    
+    // Set prevTimestamp for use in next frame
+    prevTimestamp = timestamp
+    
+    notify({ deltaY, direction, velocity })
+  }
+
+  function onScroll(event) {
+    if (!ticking.curret) {
+      ticking.current = true
+      requestAnimationFrame(timestamp => {
+        update(timestamp)
+        ticking.current = false
+      })
+    }
+  }
+  
+  function onScrollEnd(event) {
+    scrollStart = element.scrollTop
+    notify({ deltaY, direction, velocity: 0 })
+  }
+
+  useEffect(() => {
+    const element = scrollRef.current
+    if (!element) {
+      return
+    }
+    element.addEventListener('scroll', onScroll, { passive: true })
+    element.addEventListener("scrollend", onScrollEnd, { passive: true })
+    return () => {
+      element.removeEventListener('scroll', onScroll)
+      element.removeEventListener('scrollend', onScrollEnd)
     }
   }, [])
   
