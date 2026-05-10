22a5d4fbaf1ea29312d1d8249065898f05beacdd
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-10 15:42:54 -0400

    Update ScrollProvider.js

--- src/components/Providers/ScrollProvider.js
+++ src/components/Providers/ScrollProvider.js
@@ -11,7 +11,7 @@ const ScrollProvider = ({ children }) => {
   const prevTimestamp = useRef(0)
   const ticking = useRef(false)
   const scrollRef = useRef(null)
-  useScrollIntercept(scrollRef)
+  useScrollIntercept(scrollRef.current)
   const setScrollRef = useCallback(node => {
     if (node && scrollRef.current !== node) {
       scrollRef.current = node  
