7c9dbb1280201d1824be6f7f23856f2f0770cf71
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-10 12:23:51 -0400

    Update FullscreenButton.js

--- src/features/Feed/FullscreenButton/FullscreenButton.js
+++ src/features/Feed/FullscreenButton/FullscreenButton.js
@@ -1,11 +1,9 @@
-import { useRef } from 'react'
 import MaximizeIcon from '../../../components/Icons/MaximizeIcon/MaximizeIcon'
 import MinimizeIcon from '../../../components/Icons/MinimizeIcon/MinimizeIcon'
 import styles from './FullscreenButton.module.css'
 
 function FullscreenButton({ isFullScreen, enterFullScreen, exitFullScreen }) {
-  const buttonRef = useRef(null)
-  
+
   const onClick = event => {
     navigator.vibrate?.(50)
     !document.fullscreenElement ? enterFullScreen() : exitFullScreen() 
@@ -15,7 +13,7 @@ function FullscreenButton({ isFullScreen, enterFullScreen, exitFullScreen }) {
   // have aria pressed, and label accordingly.
   
   return (
-    <button className={styles.fullscreenButton} ref={buttonRef} onClick={onClick} type="button" aria-label="enter fullscreen mode">
+    <button className={styles.fullscreenButton} onClick={onClick} type="button" aria-label="enter fullscreen mode">
       {isFullScreen ? <MinimizeIcon /> : <MaximizeIcon />}
     </button>
   )
