16c17b5c2a864a199de5ac1391241365372f4d91
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-10 10:09:39 -0400

    Create useScrollIntercept.js

---
+++ src/hooks/useScrollIntercept.js
@@ -0,0 +1,41 @@
+import useScroll from './useScroll'
+
+function useScrollIntercept() {
+  const { scrollRef } = useScroll()
+  const multiplier = 0.2
+  
+  const handleManualScroll = delta => {
+    scrollRef.current.scrollTop += delta * multiplier
+  }
+
+  // 1. Mouse/Trackpad
+  window.addEventListener('wheel', event => {
+    event.preventDefault()
+    handleManualScroll(event.deltaY)
+  }, { passive: false })
+  
+  // 2. Touch (Mobile)
+  let touchStart
+  window.addEventListener('touchstart', event => {
+    touchStart = event.touches[0].pageY
+  }, { passive: false })
+
+  window.addEventListener('touchmove', event => {
+    event.preventDefault()
+    const touchEnd = event.touches[0].pageY
+    const delta = touchStart - touchEnd
+    handleManualScroll(delta)
+    touchStart = touchEnd // Update for continuous movement
+  }, { passive: false })
+  
+  // 3. Keyboard
+  window.addEventListener('keydown', event => {
+    const keys = { 'ArrowDown': 40, 'ArrowUp': -40, ' ': 200, 'PageDown': 400, 'PageUp': -400 }
+    if (keys[event.key]) {
+      event.preventDefault()
+      handleManualScroll(keys[event.key])
+    }
+  })
+}
+
+export default useScrollIntercept
