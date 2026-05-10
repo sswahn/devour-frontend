85c1cff30b19a36af03cad80eac86613e027b0ae
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-10 13:56:12 -0400

    Update useScrollIntercept.js

--- src/hooks/useScrollIntercept.js
+++ src/hooks/useScrollIntercept.js
@@ -2,36 +2,37 @@ import useScroll from './useScroll'
 
 function useScrollIntercept() {
   const { scrollRef } = useScroll()
-  const multiplier = 0.2
   
-  const handleScroll = delta => {
-    scrollRef.current.scrollTop += delta * multiplier
+  const handleScroll = deltaY => {
+    if (!ticking) {
+      ticking = true
+      requestAnimationFrame(() => {
+        scrollRef.current.scrollTop += deltaY * 0.2
+        ticking = false
+      })
+    }
   }
 
   // 1. Mouse/Trackpad
   window.addEventListener('wheel', event => {
     event.preventDefault()
-    if (!ticking) {
-      ticking = true
-      requestAnimationFrame(() => {
-        handleScroll(event.deltaY)
-        ticking = false
-      })
-    }
+    handleScroll(event.deltaY)
   }, { passive: false })
   
   // 2. Touch (Mobile)
-  let touchStart
+  let startY = 0
+  let lastY = 0
   window.addEventListener('touchstart', event => {
-    touchStart = event.touches[0].pageY
+    startY = event.touches[0].pageY
+    lastY = startY
   }, { passive: false })
 
   window.addEventListener('touchmove', event => {
     event.preventDefault()
-    const touchEnd = event.touches[0].pageY
-    const delta = touchStart - touchEnd
-    handleManualScroll(delta)
-    touchStart = touchEnd // Update for continuous movement
+    const y = event.touches[0].pageY
+    const delta = lastY - y
+    handleScroll(deltaY)
+    lastY = y
   }, { passive: false })
   
   // 3. Keyboard
@@ -39,7 +40,7 @@ function useScrollIntercept() {
     const keys = { 'ArrowDown': 40, 'ArrowUp': -40, ' ': 200, 'PageDown': 400, 'PageUp': -400 }
     if (keys[event.key]) {
       event.preventDefault()
-      handleManualScroll(keys[event.key])
+      handleScroll(keys[event.key])
     }
   })
 }
