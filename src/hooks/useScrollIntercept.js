48f1820433cc78c132521760b673a319f3973fcf
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-10 18:23:44 -0400

    Update useScrollIntercept.js

--- src/hooks/useScrollIntercept.js
+++ src/hooks/useScrollIntercept.js
@@ -6,8 +6,8 @@ function useScrollIntercept() {
   const targetScroll = useRef(0)
   const prevY = useRef(0)
   const frame = useRef(null)
-  const MULTIPLIER = 0.2 // Slows scroll - if to slow, try 0.3
-  const SMOOTHING = 0.08 // Lower: heavier, thicker, more delayed, Higher: tighter, more responsive - try 0.12 if change above
+  const MULTIPLIER = 0.9 //0.2 // Slows scroll - if to slow, try 0.3
+  const SMOOTHING = 0.25 // 0.08 // Lower: heavier, thicker, more delayed, Higher: tighter, more responsive - try 0.12 if change above
   const MAX_STEP = 80
 
   const setTargetElement = element => {
