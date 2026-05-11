ec64d6720f4ad4b8505838883377487454241276
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-11 18:07:22 -0400

    Update Profile.js

--- src/features/Profile/Profile.js
+++ src/features/Profile/Profile.js
@@ -69,7 +69,7 @@ function Profile() {
         </nav>
         <header>
           <div>
-            <ImageField src={profile.image} alt={`${profile.username}'s profile picture`} />
+            <img src={profile.image} alt={`${profile.username}'s profile picture`} />
           </div>
           <div>
             <h1 id="username">{profile.username}</h1>
