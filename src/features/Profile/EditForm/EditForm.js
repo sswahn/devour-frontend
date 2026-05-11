cac1cf409d11c8f7adb9812b8726cad261d48de3
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-11 16:54:11 -0400

    Update EditForm.js

--- src/features/Profile/EditForm/EditForm.js
+++ src/features/Profile/EditForm/EditForm.js
@@ -1,14 +1,17 @@
 
-function EditForm() {
+function EditForm({ username, location, biography }) {
 
   return (
     <form onSubmit={onSubmit}>
+
+    
       <label htmlFor="username">Username:</label>
       <input id="username" type="text" />
+      <input id="username" type="text" defaultValue={username} aria-label="update your username" />
       <label htmlFor="location">Location:</label>
-      <input id="location" type="text" />
+      <input id="location" type="text" defaultValue={location} aria-label="update your location" />
       <label htmlFor="biography">Bio:</label>
-      <input id="biography" type="text" />
+      <input id="biography" type="text" defaultValue={biography} aria-label="update your bio" />
     </form>
   )
 }
