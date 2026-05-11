ae507e435ace3f34939ba8be3c6698a83e5ad9bd
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-11 11:52:25 -0400

    Revise notification methods and remove text_data
    Updated confirmation messages for notification actions and removed unused text_data export.

--- src/features/Notifications/config.js
+++ src/features/Notifications/config.js
@@ -2,8 +2,8 @@ import FlagIcon from '../../components/Icons/FlagIcon/FlagIcon'
 import XmarkIcon from '../../components/Icons/UserXmarkIcon/UserXmarkIcon'
 
 export const dropdown = [
-  { icon: <FlagIcon />, text: 'Flag notification', method: () => confirm('Report profile?') },
-  { icon: <XmarkIcon />, text: 'Delete notification', method: () => confirm('Block user?') }
+  { icon: <FlagIcon />, text: 'Flag notification', method: () => confirm('Flag notification?') },
+  { icon: <XmarkIcon />, text: 'Delete notification', method: () => confirm('Delete notification?') }
 ]
 
 // delete this:
