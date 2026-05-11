5ec85883292e98972cebdd53a91b6f7864926e15
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-11 08:08:47 -0400

    Update EditImageButton.js

--- src/features/Profile/EditImageButton/EditImageButton.js
+++ src/features/Profile/EditImageButton/EditImageButton.js
@@ -1,22 +1,16 @@
-import XmarkIcon from '../../../components/Icons/XmarkIcon/XmarkIcon'
 import EditIcon from '../../../components/Icons/EditIcon/EditIcon'
 import styles from './EditImageButton.module.css'
 
-function EditImageButton({ field, isOpen, open, close }) {
+function EditImageButton({ open }) {
 
   const onClick = event => {
     navigation.vibrate?.()
-    isOpen ? close() : open()
+    open()
   }
 
   return (
-    <button 
-      className={styles.editImageButton} 
-      onClick={onClick} 
-      type="button" 
-      aria-pressed={isOpen}
-      aria-label={!isOpen ? `edit profile ${field}` : `close to update ${field}`}>
-      {!isOpen ? <EditIcon size={16} /> : <XmarkIcon size={16} />}
+    <button className={styles.editImageButton} onClick={onClick} type="button" aria-label="edit profile image">
+      <EditIcon size={16} />
     </button>
   )
 }
