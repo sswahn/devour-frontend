d762d7cccc7eed699f5ca078be2b3c5ad204b4b1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-11 18:33:37 -0400

    Update EditForm.js

--- src/features/Profile/EditForm/EditForm.js
+++ src/features/Profile/EditForm/EditForm.js
@@ -72,6 +72,7 @@ function EditForm({ picture, username, location, biography }) {
       <input id="location" type="text" name="location" defaultValue={location} aria-label="update your location" />
       <label htmlFor="biography">Bio:</label>
       <textarea id="biography" name="biography" rows="2" aria-label="update your bio">{biography}</textarea>
+      <button type="submit">Submit</button>
     </form>
   )
 }
