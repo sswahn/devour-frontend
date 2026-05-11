5faff3ca30c954b54e452090585d69409a9f200c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-11 17:00:17 -0400

    Update EditForm.js

--- src/features/Profile/EditForm/EditForm.js
+++ src/features/Profile/EditForm/EditForm.js
@@ -2,8 +2,10 @@ import { useRef } from 'react'
 
 function EditForm({ username, location, biography }) {
   const canvasRef = useRef(null)
+  const fileInputRef = useRef(null)
 
   const handleImageUpload = event => {
+    const files = fileInputRef.current.files
     
   }
   
@@ -14,7 +16,7 @@ function EditForm({ username, location, biography }) {
       </div>
       <div>
         <label htmlFor="upload">Select an image:</label>
-        <input id="upload" type="file"  name="upload" accept="image/*" />
+        <input id="upload" ref={fileInputRef} type="file"  name="upload" accept="image/*" />
         <button onClick={handleImageUpload} type="button">Upload Image</button>
       </div>
     
