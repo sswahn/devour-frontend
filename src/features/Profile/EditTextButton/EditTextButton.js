dcc34f0b83945bd34ab51f6c4d87efe06aede386
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-11 08:27:05 -0400

    Update EditTextButton.js

--- src/features/Profile/EditTextButton/EditTextButton.js
+++ src/features/Profile/EditTextButton/EditTextButton.js
@@ -1,1 +1,18 @@
+import EditIcon from '../../../components/Icons/EditIcon/EditIcon'
+import styles from './EditImageButton.module.css'
 
+function EditImageButton({ open, close }) {
+
+  const onClick = event => {
+    navigation.vibrate?.()
+    open()
+  }
+
+  return (
+    <button className={styles.editImageButton} onClick={onClick} type="button" aria-label="edit profile image">
+      <EditIcon size={16} />
+    </button>
+  )
+}
+
+export default EditImageButton
