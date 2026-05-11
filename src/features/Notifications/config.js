1024dc581eb57d05a377fa7bc1b4c1bc5a29dee9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-11 11:42:17 -0400

    Add dropdown options for notifications

--- src/features/Notifications/config.js
+++ src/features/Notifications/config.js
@@ -1,2 +1,7 @@
+import FlagIcon from '../../components/Icons/FlagIcon/FlagIcon'
+import XmarkIcon from '../../components/Icons/UserXmarkIcon/UserXmarkIcon'
 
-export const dropdown = []
+export const dropdown = [
+  { icon: <FlagIcon />, text: 'Flag notification', method: () => confirm('Report profile?') },
+  { icon: <XmarkIcon />, text: 'Delete notification', method: () => confirm('Block user?') }
+]
