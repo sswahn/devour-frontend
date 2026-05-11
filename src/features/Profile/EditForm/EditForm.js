b938d2d34fa8e102d5253dcd9459752c5137b542
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-11 16:51:16 -0400

    Update EditForm.js

--- src/features/Profile/EditForm/EditForm.js
+++ src/features/Profile/EditForm/EditForm.js
@@ -2,7 +2,14 @@
 function EditForm() {
 
   return (
-    <></>
+    <form onSubmit={onSubmit}>
+      <label htmlFor="username">Username:</label>
+      <input id="username" type="text" />
+      <label htmlFor="location">Location:</label>
+      <input id="location" type="text" />
+      <label htmlFor="biography">Bio:</label>
+      <input id="biography" type="text" />
+    </form>
   )
 }
 
