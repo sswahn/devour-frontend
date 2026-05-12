c6a0f255b24f20ca9f42c9ad6cd4a37baa9c1dbb
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-12 01:48:46 -0400

    Add styles to TopNav component

--- src/features/Profile/TopNav/TopNav.js
+++ src/features/Profile/TopNav/TopNav.js
@@ -1,9 +1,14 @@
 
+import styles from './TopNav.module.css'
 
 function TopNav() {
 
   return (
-    
+    <nav className={styles.topNav}>
+      <CloseButton overlay={overlay.profile} close={closeOverlay} />
+      <Dropdown items={dropdown} />
+      <EditButton open={openEditor} /> 
+    </nav>
   )
 }
 
