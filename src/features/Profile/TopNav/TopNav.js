a2d0a28866c087542b97f5a8ed42cfd58187d9b0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-12 01:51:44 -0400

    Update TopNav.js

--- src/features/Profile/TopNav/TopNav.js
+++ src/features/Profile/TopNav/TopNav.js
@@ -1,13 +1,23 @@
-
+import SearchIcon from '../../components/Icons/SearchIcon/SearchIcon'
+import MessageIcon from '../../components/Icons/MessageIcon/MessageIcon'
+import FlagIcon from '../../components/Icons/FlagIcon/FlagIcon'
+import UserXmarkIcon from '../../components/Icons/UserXmarkIcon/UserXmarkIcon'
 import styles from './TopNav.module.css'
 
-function TopNav() {
+function TopNav({ openEditor }) {
+
+  const dropdown = [
+    { icon: <EditIcon />, text: 'Edit profile', method: () => openEditor() },
+    { icon: <SearchIcon />, text: 'Search profile', method: () => alert('Search profile.') },
+    { icon: <MessageIcon />, text: 'Send message', method: () => alert('DM opened.') },
+    { icon: <FlagIcon />, text: 'Flag profile', method: () => confirm('Report profile?') },
+    { icon: <UserXmarkIcon />, text: 'Block user', method: () => confirm('Block user?') }
+  ]
 
   return (
     <nav className={styles.topNav}>
       <CloseButton overlay={overlay.profile} close={closeOverlay} />
       <Dropdown items={dropdown} />
-      <EditButton open={openEditor} /> 
     </nav>
   )
 }
