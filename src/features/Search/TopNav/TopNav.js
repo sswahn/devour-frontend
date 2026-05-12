7461139f67e212eb9d1db29c5830d618f68baca5
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-12 11:43:57 -0400

    Update TopNav.js

--- src/features/Search/TopNav/TopNav.js
+++ src/features/Search/TopNav/TopNav.js
@@ -1,8 +1,19 @@
-
+import styles from './TopNav.module.css'
 
 function TopNav() {
+
+  // might need something other than search filters
+  const dropdown = [
+    { icon: '', text: 'filter 1', method: () => console.log('filter 1') },
+    { icon: '', text: 'filter 2', method: () => console.log('filter 2') }
+  ]
+  
   return (
-    
+    <nav className={styles.topNav}>
+      <CloseButton overlay={overlay.search} close={action} />
+      {/* dont forget dropdown requires icons too. */}
+      <Dropdown items={dropdown} />
+    </nav>
   )
 }
 
