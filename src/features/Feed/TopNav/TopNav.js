18fa950ac855be8351c8ff5c928e667219b9e000
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-12 02:38:09 -0400

    Update TopNav.js

--- src/features/Feed/TopNav/TopNav.js
+++ src/features/Feed/TopNav/TopNav.js
@@ -1,12 +1,20 @@
 import CloseButton from '../../../components/CloseButton/CloseButton'
 import Dropdown from '../../../components/Dropdown/Dropdown'
+import FlagIcon from '../../../components/Icons/FlagIcon/FlagIcon'
+import UserXmarkIcon from '../../../components/Icons/UserXmarkIcon/UserXmarkIcon'
 import styles from './TopNav.module.css'
 
 function TopNav({ isFullScreen, exitFullScreen }) {
+
+  const dropdown = [
+    { icon: <FlagIcon />, text: 'Flag content', method: () => confirm('Report content?') },
+    { icon: <UserXmarkIcon />, text: 'Block user', method: () => confirm('Block user?') }
+  ]
+  
   return (
     <nav className={`topNav ${styles.topNav} ${isFullScreen ? styles.fullScreen : ''}`} aria-label="top menu">
       {isFullScreen && <CloseButton overlay="feed overlay" close={exitFullScreen} />}
-      <Dropdown items={[1,2,3]} />
+      <Dropdown items={dropdown} />
     </nav>
   )
 }
