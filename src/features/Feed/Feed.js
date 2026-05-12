95a25be1b298685301a1fbea9d33a67cb389712b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-12 03:10:41 -0400

    Update Feed.js

--- src/features/Feed/Feed.js
+++ src/features/Feed/Feed.js
@@ -17,6 +17,12 @@ function Feed() {
     { video: 3, caption: 'test 3' }
   ])
 
+  const setObserver = node => {
+    if (node) {
+      observe(node, () => console.log(`Observed: `))
+    }
+  }
+
   useEffect(() => {
     
     return () => {
@@ -26,7 +32,7 @@ function Feed() {
   
   return (
     <section ref={setScrollRef} className={styles.feed} role="feed">
-      {data.map((item, index) => <FeedNode key={index} item={item} index={index + 1} count={data.length} />)}
+      {data.map((item, index) => <FeedNode key={index} ref={setObserver} item={item} index={index + 1} count={data.length} />)}
       
       {/* <Sentinel onVisible={loadMoreData} /> */}
     </section>
