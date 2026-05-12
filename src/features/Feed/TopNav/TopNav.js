95f4cb60d910e18ca104e54dd59834842f5bd27d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-12 02:41:35 -0400

    Update TopNav.js

--- src/features/Feed/TopNav/TopNav.js
+++ src/features/Feed/TopNav/TopNav.js
@@ -1,5 +1,6 @@
 import CloseButton from '../../../components/CloseButton/CloseButton'
 import Dropdown from '../../../components/Dropdown/Dropdown'
+import UserPlusIcon from '../../../components/Icons/UserPlusIcon/UserPlusIcon'
 import FlagIcon from '../../../components/Icons/FlagIcon/FlagIcon'
 import UserXmarkIcon from '../../../components/Icons/UserXmarkIcon/UserXmarkIcon'
 import styles from './TopNav.module.css'
@@ -7,6 +8,7 @@ import styles from './TopNav.module.css'
 function TopNav({ isFullScreen, exitFullScreen }) {
 
   const dropdown = [
+    { icon: <UserPlusIcon />, text: 'Follow user', method: () => alert('User followed?') },
     { icon: <FlagIcon />, text: 'Flag content', method: () => confirm('Report content?') },
     { icon: <UserXmarkIcon />, text: 'Block user', method: () => confirm('Block user?') }
   ]
