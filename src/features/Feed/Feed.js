52c1032d8eb348138d95af09f3c081226166298a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-12 03:34:53 -0400

    Update Feed.js

--- src/features/Feed/Feed.js
+++ src/features/Feed/Feed.js
@@ -17,14 +17,15 @@ function Feed() {
     { video: 3, caption: 'test 3' }
   ])
 
-  const observerCallback = () => {
+  const observerCallback = node => {
     // handle Roving Index, etc.
+    node.tabIndex = -1
     console.log('feed node observer called.')
   }
 
   const setObserver = node => {
     if (node) {
-      observe(node, () => observerCallback())
+      observe(node, () => observerCallback(node))
     }
   }
 
@@ -36,7 +37,7 @@ function Feed() {
   
   return (
     <section ref={setScrollRef} className={styles.feed} role="feed">
-      {data.map((item, index) => <FeedNode key={index} ref={setObserver} item={item} index={index + 1} count={data.length} />)}
+      {data.map((item, index) => <FeedNode key={index} setObserver={setObserver} item={item} index={index + 1} count={data.length} />)}
       
       {/* <Sentinel onVisible={loadMoreData} /> */}
     </section>
