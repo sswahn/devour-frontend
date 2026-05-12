3007fe00b11545fcd1a4119af154faa6eb92c01e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-12 02:46:10 -0400

    Add AlignLeftIcon to TopNav dropdown options

--- src/features/Feed/TopNav/TopNav.js
+++ src/features/Feed/TopNav/TopNav.js
@@ -1,5 +1,6 @@
 import CloseButton from '../../../components/CloseButton/CloseButton'
 import Dropdown from '../../../components/Dropdown/Dropdown'
+import AlignLeftIcon from '../../../components/Icons/AlignLeftIcon/AlignLeftIcon'
 import UserPlusIcon from '../../../components/Icons/UserPlusIcon/UserPlusIcon'
 import FlagIcon from '../../../components/Icons/FlagIcon/FlagIcon'
 import UserXmarkIcon from '../../../components/Icons/UserXmarkIcon/UserXmarkIcon'
@@ -8,7 +9,8 @@ import styles from './TopNav.module.css'
 function TopNav({ isFullScreen, exitFullScreen }) {
 
   const dropdown = [
-    { icon: <UserPlusIcon />, text: 'Follow user', method: () => alert('User followed?') },
+    { icon: <AlignLeftIcon />, text: 'Description', method: () => alert('Read post description.') },
+    { icon: <UserPlusIcon />, text: 'Follow', method: () => alert('User followed.') },
     { icon: <FlagIcon />, text: 'Flag content', method: () => confirm('Report content?') },
     { icon: <UserXmarkIcon />, text: 'Block user', method: () => confirm('Block user?') }
   ]
