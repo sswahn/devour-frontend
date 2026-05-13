61feefd91bef18bfc20e71bbcae8667912e39e66
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-13 13:22:35 -0400

    Create useValidation.js

---
+++ src/hooks/useValidation.js
@@ -0,0 +1,17 @@
+
+
+function useValidation(values) {
+  const patterns = {
+    username: /^[\p{L}\p{N}](?:[\p{L}\p{N}_]*[\p{L}\p{N}])?$/u,
+    contact: '', // email or tel
+  }
+
+  const validate = value => {
+    if (!patterns.text(regex) {
+
+    }
+  }
+
+}
+
+export default useValidation
