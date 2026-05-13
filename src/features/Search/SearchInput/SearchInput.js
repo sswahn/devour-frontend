0c3ea81df1f537a033cbf85e5dd28d4d0e717e88
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-13 17:44:10 -0400

    Update SearchInput.js

--- src/features/Search/SearchInput/SearchInput.js
+++ src/features/Search/SearchInput/SearchInput.js
@@ -1,15 +1,15 @@
 import { useState, useRef, useEffect } from 'react'
 import styles from './SearchInput.module.css'
 
-function SearchInput({ searchValue, errorMessage, setSearchValue, setErrorMessage }) {
+function SearchInput({ searchValue, error, setSearchValue, setError }) {
   const inputRef = useRef(null)
   
   const onChange = ({ target }) => {
     if (target.validity.patternMismatch) {
       target.reportValidity()
-      setErrorMessage(true)
+      setError(true)
     } else if (error) {
-      setErrorMessage(false)
+      setError(false)
     }
     setSearchValue(target.value)
   }
