10fe1af1dc2e5c50776ac5c8e2af377059f4c6c6
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-13 15:30:41 -0400

    Update useValidation.js

--- src/hooks/useValidation.js
+++ src/hooks/useValidation.js
@@ -3,21 +3,32 @@
 function useValidation() {
 
   const validateSearchTerm = term => {
-    const regex = /^[^<>()\[\]\\/|;=~%^]+$/
     if (term.length < 3 || term.length > 100) {
       throw new Error('Search query must be between 3 and 100 characters.')
     }
+    const regex = /^[^<>()\[\]\\/|;=~%^]+$/
     if (!regex.test(term)) {
       throw new Error('Special characters are not allowed.')
     }
     return term
   }
 
+  const validateComment = comment => {
+    if (comment.length < 3 || comment.length > 1000) {
+      throw new Error('Comments must be between 3 and 1000 characters.')
+    }
+    const xssHtmlRegex = /<[^>]*>/
+    if (xssHtmlRegex.test(comment)) {
+      throw new Error('HTML tags and scripts are not allowed in comments.')
+    }
+    return comment
+  }
+
   const validateUsername = username => {
-    const regex = /^[\p{L}\p{N}](?:[\p{L}\p{N}_]*[\p{L}\p{N}])?$/u
     if (username.length < 3 || username.length > 30) {
       throw new Error('Username must be between 3 and 30 characters.')
     }
+    const regex = /^[\p{L}\p{N}](?:[\p{L}\p{N}_]*[\p{L}\p{N}])?$/u
     if (!regex.test(username)) {
       throw new Error('Please use a valid username format.')
     }
@@ -25,14 +36,14 @@ function useValidation() {
   }
 
   const validateContact = contact => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/u
-    const phoneRegex = /^\+?\(?\d{1,4}\)?[\s.-]?\(?\d{1,4}\)?[\s.-]?\d{3,4}[\s.-]?\d{3,4}$/
     if (contact.length < 3 || contact.length > 254) {
       throw new Error('Contact must be a valid length.')
     }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/u
     if (emailRegex.test(contact)) {
       return contact
     }
+    const phoneRegex = /^\+?\(?\d{1,4}\)?[\s.-]?\(?\d{1,4}\)?[\s.-]?\d{3,4}[\s.-]?\d{3,4}$/
     if (phoneRegex.test(contact)) {
       const totalDigits = value.replace(/\D/g, '').length;
       if (totalDigits >= 7 && totalDigits <= 15) {
