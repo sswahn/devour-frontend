3da57c4f5e6ffbca7141015db6f922cf2f35808d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-13 09:52:31 -0400

    Update Register.js

--- src/features/Register/Register.js
+++ src/features/Register/Register.js
@@ -67,7 +67,8 @@ function Register() {
             inputMode="email"
             autoComplete="username webauthn"
             autoCapitalize="none"
-            pattern="(?:[^@\s]+@[^@\s]+\.[^@\s]+)|(?:\+?[\d\s\-()]{7,15})"
+           // pattern="(?:[^@\s]+@[^@\s]+\.[^@\s]+)|(?:\+?[\d\s\-()]{7,15})"
+            pattern="[^@\s]+@[^@\s]+\.[^@\s]+|\+?[\d\s\-()]{7,15}"
             title="Please enter a valid email address or an international phone number (e.g., +1 234 567 8900)."
             required />
           <button type="submit" disbled={!!message || loading}>Sign Up</button>
