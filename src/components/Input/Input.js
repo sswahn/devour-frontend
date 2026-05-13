a1edeffb58e255cbf0abf6dc7beaea53ac35742b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-13 09:42:45 -0400

    Update Input.js

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -7,7 +7,9 @@ const Input = ({ id, type, label, inputMode, autoComplete, error, required, ...p
 
   const focusInvalidInput = () => {
     const form = inputRef.current?.closest('form')
-    const input = Array.from(form.elements).find(element => element.hasAttribute('aria-invalid'))
+    const input = Array.from(form.elements).find(
+      input => input.hasAttribute('aria-invalid')
+    )
     input.focus()
   }
 
