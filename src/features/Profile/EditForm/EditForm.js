5bb0de551d65109c4b8e79c00f7643e2ae93a534
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-14 11:52:01 -0400

    Update EditForm.js

--- src/features/Profile/EditForm/EditForm.js
+++ src/features/Profile/EditForm/EditForm.js
@@ -42,8 +42,8 @@ function EditForm({ profile, setProfile }) {
   const loadImage = event => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
-    canvas.width = image.width
-    canvas.height = image.height
+    canvas.width = 170 // image.width
+    canvas.height = 170 // image.height
     ctx.drawImage(image, 0, 0)
     URL.revokeObjectURL(image.src)
   }
