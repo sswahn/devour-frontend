37935b048aea1ac9712d2df82d463513a2d02374
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-15 08:15:33 -0400

    Update Register.js

--- src/features/Register/Register.js
+++ src/features/Register/Register.js
@@ -74,6 +74,7 @@ function Register() {
           <SubmitButton disabled={loading || !!message} />
           {message && <div className={styles.success} role="alert">{message}</div>}
           {errorMessage && <div className={styles.danger} role="alert">{errorMessage}</div>}
+          {/* show Login Button with border and no bg */}
         </form>
       </div>
     </section>
