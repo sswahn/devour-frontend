b8a50f7be0deb1b89088824c971919d32e6607b1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-15 14:40:54 -0400

    Update Providers.js

--- src/Providers.js
+++ src/Providers.js
@@ -1,5 +1,6 @@
 import { ScrollProvider } from './components/Providers/ScrollProvider'
 import { SessionProvider } from './components/Providers/SessionProvider'
+import { FullscreenProvider } from './components/Providers/FullscreenProvider'
 import { OverlayProvider } from './components/Providers/OverlayProvider'
 import { ProfileProvider } from './components/Providers/ProfileProvider'
 import { DialogProvider } from './components/Providers/DialogProvider'
@@ -9,15 +10,17 @@ function Providers({ children }) {
   return (
     <ScrollProvider>
       <SessionProvider>
-        <OverlayProvider>
-          <ProfileProvider>
-            <DialogProvider>
-              <ContentProvider>
-                {children}
-              </ContentProvider>
-            </DialogProvider>
-          </ProfileProvider>
-        </OverlayProvider>
+        <FullscreenProvider>
+          <OverlayProvider>
+            <ProfileProvider>
+              <DialogProvider>
+                <ContentProvider>
+                  {children}
+                </ContentProvider>
+              </DialogProvider>
+            </ProfileProvider>
+          </OverlayProvider>
+        </FullscreenProvider>
       </SessionProvider>
     </ScrollProvider>
   )
