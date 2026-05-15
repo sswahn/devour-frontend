68276ce22786ebf17f5b842d6e1571ce7668636c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-15 14:34:45 -0400

    Update FullscreenProvider.js

--- src/components/Providers/FullscreenProvider.js
+++ src/components/Providers/FullscreenProvider.js
@@ -1,9 +1,20 @@
-import { useState, createContext } from 'react'
+import { useState, useEffect, createContext } from 'react'
 
 const FullScreenContext = createContext(null)
 
 function FullscreenProvider({ children }) {
-  const [isFullscreen, setFullscreen] = useState()
+  const [isFullscreen, setFullscreen] = useState(false)
+
+  const onFullscreenChange = event => {
+    setState(!!document.fullscreenElement)
+  }
+
+  useEffect(() => {
+    document.addEventListener('fullscreenchange', onFullscreenChange)
+    return () => {
+      document.removeEventListener('fullscreenchange', onFullscreenChange)
+    }
+  }, [])
   
   return (
     <FullScreenContext.Provider value={isFullscreen}>
