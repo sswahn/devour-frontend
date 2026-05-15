7013506734ae41dd390a66c7ee87eadf61ca2c5a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-15 07:51:00 -0400

    Update Search.js

--- src/features/Search/Search.js
+++ src/features/Search/Search.js
@@ -37,14 +37,7 @@ function Search() {
   return (
     <search id={overlay.search} className={styles.search} ref={overlayRef} onKeyDown={onKeyDown} role="dialog" aria-modal="true">
       <div onPointerDown={onPointerDown} onPointerMove={onPointerMove} onPointerUp={onPointerUp} onPointerCancel={onPointerCancel}>
-        <nav>
-          <CloseButton overlay={overlay.search} close={action} />
-          {/* dont forget dropdown requires icons too. */}
-          <Dropdown items={[
-            { text: 'alert message', method: () => alert('dropdown item clicked.') },
-            { text: 'console log message', method: () => console.log('dropdown item clicked.') }
-          ]} />
-        </nav>
+        <TopNav />
         <form onSubmit={onSubmit}>
           <SearchIcon size={10} />
           <SearchInput 
