b0b0d05f808fd988a8bcbc8830278d85a09fbf8c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-16 10:23:42 -0400

    Update Feed.js

--- src/features/Feed/Feed.js
+++ src/features/Feed/Feed.js
@@ -3,15 +3,12 @@ import { api } from '../../config'
 import createObserver from '../../utilities/observer'
 import server from '../../utilities/server'
 import database from '../../utilities/database'
-import useScroll from '../../hooks/useScroll'
 import FeedNode from './FeedNode/FeedNode'
 import Sentinel from './Sentinel/Sentinel' 
 import styles from './Feed.module.css'
 
 function Feed() {
-  const { setScrollRef } = useScroll()
   const { observe, unobserve, disconnect } = createObserver()
-  const prevNode = useRef(null)
   const [data, setData] = useState([
     { video: 1, caption: 'test 1' },
     { video: 2, caption: 'test 2' },
@@ -38,7 +35,7 @@ function Feed() {
   }, [])
   
   return (
-    <section ref={setScrollRef} className={styles.feed} role="feed">
+    <section className={styles.feed} role="feed">
       {data.map((item, index) => <FeedNode key={index} item={item} index={index + 1} count={data.length} />)}
       
       {/* <Sentinel onVisible={loadMoreData} /> */}
