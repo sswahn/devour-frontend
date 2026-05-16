0b79faeb8f97e83fce6c0b43de4bc5bbbe9620e5
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-16 10:37:25 -0400

    Enhance error logging in ErrorBoundary component
    Added logging for error information in componentDidCatch.

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -13,6 +13,10 @@ class ErrorBoundary extends Component {
   }
 
   componentDidCatch(error, info) {
+
+    console.log("React Component Stack:", errorInfo.componentStack)
+    console.log("Original Error:", error.stack)
+    
     logError(error, {
       source: 'react.errorBoundary',
       componentStack: info.componentStack
@@ -28,7 +32,7 @@ class ErrorBoundary extends Component {
           background: 'black',
           color: 'white',
           height: '100%',
-          padding: '200px 0 0 200px',
+          padding: '200px 0 0 8px',
           width: '100%',
         }}>
           <h1>Oops! Something went wrong.</h1>
