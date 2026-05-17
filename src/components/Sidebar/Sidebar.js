ade42369582f16dc28b63299d5a28ed842c14d90
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-17 14:23:05 -0400

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,6 +3,13 @@ import CloseButton from './CloseButton/CloseButton'
 import styles from './Sidebar.module.css'
 
 function Sidebar({ sidebarRef, content, isOpen, close }) {
+
+  const onClick = event => {
+    if (event.target === event.currentTarget) {
+      close()
+    }
+  }
+  
   return (
     <aside className={styles.sidebar} ref={sidebarRef}>
       <div className={`${isOpen ? styles.open : null}`}>
