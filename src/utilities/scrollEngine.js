1158ca5b6fd0e7f323569318fe3863b38bcbb38b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-17 09:49:00 -0400

    Update scrollEngine.js

--- src/utilities/scrollEngine.js
+++ src/utilities/scrollEngine.js
@@ -32,15 +32,6 @@ function update(timestamp) {
   const deltaTime = timestamp - prevTimestamp
   const velocity = dY / deltaTime
 
-  // Pass raw velocity from here, and use smoothed in implementation within component
-  // Formula: (currentRawVelocity * smoothingFactor) + (PreviousSmoothedVelocity * (1 - Factor))
-  // (smoothingFactor: 0 < factor <= 1. Smaller = smoother.
-  //velocity = (rawVelocity * 0.05) + (velocity * (1 - 0.05))
-  // try for a buttery scroll:
-  //velocity = (rawVelocity * 0.03) + (velocity * 0.97)
-
-  console.log('velocity: ', velocity)
-  
   // Set prevTimestamp for use in next frame
   prevTimestamp = timestamp
   
