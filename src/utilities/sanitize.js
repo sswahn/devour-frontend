2422a6fc61c5839226f4185cd98bcb935a929b99
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-17 08:14:53 -0400

    Create sanitize.js

---
+++ src/utilities/sanitize.js
@@ -0,0 +1,23 @@
+
+export const sanitize = (value, length = 1000) => {
+
+  // 1. Standard social app length check
+  if (value.length < 3 || value.length > length) {
+    throw new Error(`Comments must be between 3 and ${length} characters.`)
+  }
+
+  // 2. Map dangerous characters to safe HTML entities
+  const htmlEntities = {
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    '/': '&#x2F;',
+    '&': '&amp;'
+  }
+
+  // 3. Replace characters using a secure global regex
+  const sanitizedValue = value.replace(/[<>"'&/]/g, match => htmlEntities[match])
+
+  return sanitizedValue
+}
