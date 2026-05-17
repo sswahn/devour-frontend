efa045ec1ed020efa45ed671d9672d947a373472
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-17 09:33:14 -0400

    Update TopNav.js

--- src/features/Comments/TopNav/TopNav.js
+++ src/features/Comments/TopNav/TopNav.js
@@ -1,7 +1,7 @@
 import { overlay } from '../../../config'
 import useOverlay from '../../../hooks/useOverlay'
-import BackButton from '../../components/BackButton/BackButton'
-import Dropdown from '../../components/Dropdown/Dropdown'
+import BackButton from '../../../components/BackButton/BackButton'
+import Dropdown from '../../../components/Dropdown/Dropdown'
 
 function TopNav() {
   const { closeOverlay } = useOverlay()
