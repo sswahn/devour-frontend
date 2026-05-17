792dc268f1e2e8c28b702d88d4997a8e5440c606
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-17 09:34:40 -0400

    Add styles to TopNav component

--- src/features/Comments/TopNav/TopNav.js
+++ src/features/Comments/TopNav/TopNav.js
@@ -2,13 +2,14 @@ import { overlay } from '../../../config'
 import useOverlay from '../../../hooks/useOverlay'
 import BackButton from '../../../components/BackButton/BackButton'
 import Dropdown from '../../../components/Dropdown/Dropdown'
+import styles from './TopNav.module.css'
 
 function TopNav() {
   const { closeOverlay } = useOverlay()
   const dropdown = []
   
   return (
-    <nav>
+    <nav className={styles.topNav}>
       <BackButton overlay={overlay.comments} close={closeOverlay} />
       <Dropdown items={dropdown} />
     </nav>
