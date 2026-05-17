af19a0f2c24d586563491bef584e61bd15e41cab
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-17 09:15:59 -0400

    Update SearchResults.js

--- src/features/Search/SearchResults/SearchResults.js
+++ src/features/Search/SearchResults/SearchResults.js
@@ -5,25 +5,21 @@ function SearchResults({ searchResults, loading }) {
   
   const dropdown =  []
 
-  return (
-    <ul id="suggestions" role="listbox" aria-live="polite" aria-busy={loading}>
-      {searchResults.map(result =>
-        <li key={result.id} role="option">
-          <div>
-            <Avatar username={result.username} />
-            <div>
-              <strong>{result.username}</strong>
-              <time datetime={result.timestamp}>{result.timestamp}</time>
-            </div>
-            <div>
-              <span>{result.location}</span>
-              <span>{result.cuisine}</span>
-            </div>
-            <Dropdown items={dropdown} />
-          </div>
-        </li>
-      )}    
-    </ul>  
+  return searchResults.length > 0 && searchResults.map(result =>
+    <li key={result.id} role="option">
+      <div>
+        <Avatar username={result.username} />
+        <div>
+          <strong>{result.username}</strong>
+          <time datetime={result.timestamp}>{result.timestamp}</time>
+        </div>
+        <div>
+          <span>{result.location}</span>
+          <span>{result.cuisine}</span>
+        </div>
+        <Dropdown items={dropdown} />
+      </div>
+    </li>
   )
 }
 
