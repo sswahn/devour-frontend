e952d890370ac42553c3155ce7c32ab49b0258ee
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-17 08:56:07 -0400

    Render search results or loading spinner in SearchResults
    Updated SearchResults component to render a list of search results or a loading spinner.

--- src/features/Search/SearchResults/SearchResults.js
+++ src/features/Search/SearchResults/SearchResults.js
@@ -1,7 +1,17 @@
 
 function SearchResults() {
 
-  return ()
+  return (
+    <ul id="suggestions" role="listbox" aria-live="polite" aria-busy={loading}>
+      {loading ? <LoadingSpinner /> : searchResults?.map((result, index) =>
+        <li key={index} role="option">
+          <div>
+            {result}
+          </div>
+        </li>
+      )}    
+    </ul>  
+  )
 }
 
 export default SearchResults
