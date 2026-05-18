5cd56a9b8aff125d71ec00819f4c3d747dbd5b8c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-18 12:56:10 -0400

    Update registerServiceWorker.js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,7 +2,7 @@
 export default async function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
     try {
-      const registration = await navigator.serviceWorker.register('/sw.js')
+      const registration = await navigator.serviceWorker.register('/devour-frontend/sw.js') // change this later when live to /sw.js
 
       console.log('SW registered:', registration)
     }
