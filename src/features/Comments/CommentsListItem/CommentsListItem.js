d3c9a6a2369b7b926a809152429353ec3653513c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-18 08:22:31 -0400

    Update CommentsListItem.js

--- src/features/Comments/CommentsListItem/CommentsListItem.js
+++ src/features/Comments/CommentsListItem/CommentsListItem.js
@@ -1,9 +1,20 @@
 
 
-function CommentsListItem() {
-
+function CommentsListItem({ comment }) {
   return (
-    
+    <li key={comment.id}>
+      <article>
+        <header>
+          <h4>{comment.username}</h4>
+          <time datetime={comment.datetime}>comment.datetime</time>
+          {/* <Dropdown /> */}
+        </header>
+        <p>{comment.text}</p>
+        <div>
+          {/* like button */}
+        </div>
+      </article>
+    </li>
   )
 }
 
