9e119077910d81ae535920d7c99ae934f8e3485b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-18 12:59:31 -0400

    Update registerServiceWorker.js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,13 +1,15 @@
 
 export default async function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register('/devour-frontend/sw.js') // change this later when live to /sw.js
-
-      console.log('SW registered:', registration)
-    }
-    catch (error) {
-      console.error('SW registration failed:', error)
-    }
+    window.addEventListener('load', async () => {
+      try {
+        const registration = await navigator.serviceWorker.register('/devour-frontend/sw.js') // change this later when live to /sw.js
+  
+        console.log('SW registered:', registration)
+      }
+      catch (error) {
+        console.error('SW registration failed:', error)
+      }
+    })
   }
 }
