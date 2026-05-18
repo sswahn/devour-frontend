d699219428af83aa57a16b87647d45cececf8509
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-18 09:58:27 -0400

    Update FullscreenButton.js

--- src/features/Feed/FullscreenButton/FullscreenButton.js
+++ src/features/Feed/FullscreenButton/FullscreenButton.js
@@ -1,19 +1,20 @@
+import useFullscreen from '../../../hooks/useFullscreen'
 import MaximizeIcon from '../../../components/Icons/MaximizeIcon/MaximizeIcon'
 import MinimizeIcon from '../../../components/Icons/MinimizeIcon/MinimizeIcon'
 import styles from './FullscreenButton.module.css'
 
 function FullscreenButton({ enterFullScreen, exitFullScreen }) {
-
+  const { isFullscreen } = useFullscreen()
+  
   const onClick = event => {
     navigator.vibrate?.(50)
     !isFullscreen ? enterFullScreen() : exitFullScreen() 
   }
 
-  // button needs to toggle icon from expand to contract, 
   // have aria pressed, and label accordingly.
   
   return (
-    <button className={styles.fullscreenButton} onClick={onClick} type="button" aria-label="enter fullscreen mode">
+    <button className={styles.fullscreenButton} onClick={onClick} type="button" aria-pressed={isFullscreen} aria-label="enter fullscreen mode">
       {isFullscreen ? <MinimizeIcon /> : <MaximizeIcon />}
     </button>
   )
