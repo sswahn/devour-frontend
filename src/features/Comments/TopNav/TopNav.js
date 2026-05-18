ca9e93855d1193fe9236d70355cadf0578a35a41
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-18 10:37:32 -0400

    Update TopNav.js

--- src/features/Comments/TopNav/TopNav.js
+++ src/features/Comments/TopNav/TopNav.js
@@ -3,13 +3,13 @@ import BackButton from '../../../components/BackButton/BackButton'
 import Dropdown from '../../../components/Dropdown/Dropdown'
 import styles from './TopNav.module.css'
 
-function TopNav({ closeComments }) {
+function TopNav({ close }) {
   
   const dropdown = []
   
   return (
     <nav className={styles.topNav}>
-      <BackButton overlay={overlay.comments} close={closeComments} />
+      <BackButton overlay={overlay.comments} close={close} />
       <Dropdown items={dropdown} />
     </nav>
   )
