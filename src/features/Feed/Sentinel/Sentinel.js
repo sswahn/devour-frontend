53611b98155be3053385eaef2f5ac401cc389aba
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-19 10:25:35 -0400

    Update Sentinel.js

--- src/features/Feed/Sentinel/Sentinel.js
+++ src/features/Feed/Sentinel/Sentinel.js
@@ -1,7 +1,7 @@
 import createObserver from '../../../utilities/observer'
 
 function Sentinel({ setLoadMore }) {
-  const { observe, unobserve, disconnect } = createObserver()
+  const { observe, unobserve, disconnect } = createObserver({ rootMargin = '64px' })
   
   const observerCallback = entry => { 
     if (entry.isIntersecting) {
