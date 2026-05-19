0ed2c0e0d2b41409cc22a5699f8770b62a220482
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-19 10:34:05 -0400

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,15 @@
-import { Suspense, lazy } from 'react'
+import { useState,  Suspense, lazy } from 'react'
 import Suggestions from '../Suggestions/Suggestions'
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
 const Feed = lazy(() => import('../../features/Feed/Feed'))
 import styles from './Main.module.css'
 
 function Main() {
+  const [data, setData] = useState([
+    { video: 1, caption: 'test 1' },
+    { video: 2, caption: 'test 2' },
+    { video: 3, caption: 'test 3' }
+  ])
   
   return (
     <main className={styles.main} aria-description="When text is highlighted, it will automatically be read aloud.">
@@ -12,7 +17,7 @@ function Main() {
       {/* <Suggestions /> etc. */}
 
       <Suspense fallback={<LoadingSpinner />}>
-        <Feed />
+        <Feed data={data} />
       </Suspense>
     </main>
   )
