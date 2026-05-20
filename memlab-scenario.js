eae4b3d24dbb82ce3a1b62385f9ecde2e18817ba
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-20 09:46:50 -0400

    Delete memlab-scenario.js

--- memlab-scenario.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  url: 'http://localhost:3000',
-
-  action: async function (page) {
-    await page.goto('http://localhost:3000');
-    await page.waitForSelector('body');
-  }
-};
