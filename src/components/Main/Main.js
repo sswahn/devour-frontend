7aa4e0b593de11adc7039d7ad3d7dd4d14000c30
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-20 07:12:35 -0400

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,9 +6,9 @@ import styles from './Main.module.css'
 
 function Main() {
   const [data, setData] = useState([
-    { picture: '', username: 'test_user1', video: 1, caption: 'test 1' },
-    { picture: '', username: 'test_user2',  video: 2, caption: 'test 2' },
-    { picture: '', username: 'test_user3', video: 3, caption: 'test 3' }
+    { picture: '', username: 'test_user1', video: 1, caption: 'test caption 1' },
+    { picture: '', username: 'test_user2',  video: 2, caption: 'test caption 2' },
+    { picture: '', username: 'test_user3', video: 3, caption: 'test captions 3' }
   ])
   
   return (
