5907ac1e887a0684b91d90826fe6dab72090c786
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-20 07:06:38 -0400

    Refactor TopNav component to remove unused prop
    Removed the 'exitFullScreen' prop from TopNav component.

--- src/features/Feed/TopNav/TopNav.js
+++ src/features/Feed/TopNav/TopNav.js
@@ -6,7 +6,7 @@ import FlagIcon from '../../../components/Icons/FlagIcon/FlagIcon'
 import UserXmarkIcon from '../../../components/Icons/UserXmarkIcon/UserXmarkIcon'
 import styles from './TopNav.module.css'
 
-function TopNav({ exitFullScreen }) {
+function TopNav() {
   
   const dropdown = [
     { icon: <AlignLeftIcon />, text: 'Description', method: () => alert('Read post description.') },
@@ -17,6 +17,7 @@ function TopNav({ exitFullScreen }) {
   
   return (
     <nav className={`topNav ${styles.topNav}`} aria-label="top menu">
+      {/* Not sure where to put <Avatar /> */}
       <Dropdown items={dropdown} />
     </nav>
   )
