04153e09ebf92a309b117e0d6a3b241f3dcf475e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-20 09:14:12 -0400

    Update memlab-scenario.js

--- memlab-scenario.js
+++ memlab-scenario.js
@@ -3,12 +3,7 @@ module.exports = {
   url: 'http://localhost:3000',
 
   action: async function (page) {
+    await page.goto('http://localhost:3000');
     await page.waitForSelector('body');
-  },
-
-  getHeadlessBrowserOptions: function () {
-    return {
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    };
   }
 };
