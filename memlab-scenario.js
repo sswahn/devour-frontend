d7b6bf32a3e2c0941757337c04022a2473c7578b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-20 08:51:52 -0400

    Create memlab-scenario.js

---
+++ memlab-scenario.js
@@ -0,0 +1,11 @@
+module.exports = {
+  url: 'http://localhost:3000',
+
+  action: async function (page) {
+    await page.waitForSelector('body')
+  },
+
+  back: async function (page) {
+    await page.goBack()
+  }
+}
