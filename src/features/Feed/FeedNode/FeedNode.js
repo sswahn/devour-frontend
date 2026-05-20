799b64f4b7e044e03cfa0b22bb2c36db5cc5732b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-20 07:06:05 -0400

    Update FeedNode.js

--- src/features/Feed/FeedNode/FeedNode.js
+++ src/features/Feed/FeedNode/FeedNode.js
@@ -4,7 +4,6 @@ import useGestures from '../../../hooks/useGestures'
 import useScrollLock from '../../../hooks/useScrollLock'
 import TopNav from '../TopNav/TopNav'
 import SideNav from '../SideNav/SideNav'
-import Avatar from '../../../components/Avatar/Avatar'
 import Comments from '../../Comments/Comments'
 import styles from './FeedNode.module.css'
 
@@ -82,8 +81,6 @@ function FeedNode({ item, index, count }) {
   
         {/* item.videoUrl && <video ref={ref} src={item.videoUrl} preload="metadata" muted playsInline loop /> */}
         {item.caption && <figcaption>{item.caption}</figcaption>}
-
-        {/* Not sure where to put <Avatar /> */}
          
         <SideNav 
           isDoubleTap={isDoubleTap} 
