bd07d9e047a909ae5fc08351544ba68b9f45a46b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-21 08:45:14 -0400

    Update MuteButton.js

--- src/features/Camera/MuteButton/MuteButton.js
+++ src/features/Camera/MuteButton/MuteButton.js
@@ -15,7 +15,6 @@ function MuteButton({ streamRef }) {
   return (
     <button className={styles.muteButton} onClick={toggleMute} type="button" aria-label={mute ? 'unmute' : 'mute'}>
       {mute ? <MicrophoneSlashIcon /> : <MicrophoneIcon />}
-      <div className="tooltip" role="tooltip">{mute ? 'Unmute' : 'Mute'}</div>
     </button>
   )
 }
