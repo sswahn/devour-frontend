44ce0142989c2130a614652e01d0261d0aa7f4a9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-05-30 08:09:46 -0400

    Update CommentsForm.js

--- src/features/Comments/CommentsForm/CommentsForm.js
+++ src/features/Comments/CommentsForm/CommentsForm.js
@@ -3,7 +3,20 @@ import styles from './CommentsForm.module.css'
 function CommentsForm() {
 
   return (
-    
+    <form className={styles.commentsForm} onSubmit={onSubmit} aria-label="comment form">
+      <textarea
+        id="comment"
+        name="comment"
+        spellCheck="true"
+        enterkeyhint="send"
+        required
+        placeholder="Leave a comment..."
+        aria-label="comment input">
+        </textarea>
+      <button type="submit" aria-label="submit comment">
+        <PaperPlaneIcon />    
+      </button>
+    </form>
   )
 }
 
