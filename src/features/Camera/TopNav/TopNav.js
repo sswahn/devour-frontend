880a7f0da2f4a40a67a64f0d730c290331b1864f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-03 07:36:50 -0400

    Update TopNav.js

--- src/features/Camera/TopNav/TopNav.js
+++ src/features/Camera/TopNav/TopNav.js
@@ -1,9 +1,11 @@
-
+import BackButton from '../../components/BackButton/BackButton'
+import RecordTimer from './RecordTimer/RecordTimer'
+import LightButton from './LightButton/LightButton'
 import styles from 'TopNav.module.css'
 
 function TopNav() {
   return (
-    <nav>
+    <nav className={styles.topNav}>
       <BackButton overlay={overlay.camera} close={closeCamera} />
       <RecordTimer mode={mode} timer={timer} setTimer={setTimer} stopCamera={stopCamera} />
       <LightButton streamRef={streamRef} />
