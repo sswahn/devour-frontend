f0414fba345eff07b8f2c4f9b518549228169454
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-03 07:35:56 -0400

    Replace fragment with nav element in TopNav component

--- src/features/Camera/TopNav/TopNav.js
+++ src/features/Camera/TopNav/TopNav.js
@@ -3,7 +3,11 @@ import styles from 'TopNav.module.css'
 
 function TopNav() {
   return (
-    <></>
+    <nav>
+      <BackButton overlay={overlay.camera} close={closeCamera} />
+      <RecordTimer mode={mode} timer={timer} setTimer={setTimer} stopCamera={stopCamera} />
+      <LightButton streamRef={streamRef} />
+    </nav>
   )
 }
 
