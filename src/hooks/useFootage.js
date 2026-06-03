e47a2fdb8009df0f02919745412461f401100b9e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-03 08:08:14 -0400

    Update useFootage.js

--- src/hooks/useFootage.js
+++ src/hooks/useFootage.js
@@ -2,9 +2,9 @@ import { useContext } from 'react'
 import { FootageContext } from '../components/Providers/FootageProvider'
 
 function useFootage() {
-  const { content, setContent } = useContext(FootageContext)
+  const { footage, setFootage, duration, setDuration } = useContext(FootageContext)
 
-  return { content, setContent }
+  return { footage, setFootage, duration, setDuration }
 }
 
 export default useFootage
