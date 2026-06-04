ae3b60e28628db65dc1f237ce76a7b946c92fa6e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-04 08:57:23 -0400

    Create VideoEditor.js

---
+++ src/features/VideoEditor/VideoEditor.js
@@ -0,0 +1,12 @@
+import styles from './VideoEditor.module.css'
+
+function VideoEditor() {
+
+  return (
+    <section className={styles.videoEditor}>
+      <div></div>
+    </section>
+  )
+}
+
+export default VideoEditor
