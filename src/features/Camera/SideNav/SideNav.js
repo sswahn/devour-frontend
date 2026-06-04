6c7f85129a8639ee0d83e2a933875ac95d07ada1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-04 10:57:34 -0400

    Update SideNav.js

--- src/features/Camera/SideNav/SideNav.js
+++ src/features/Camera/SideNav/SideNav.js
@@ -2,11 +2,12 @@ import EditorButton from '../EditorButton/EditorButton'
 import MuteButton from '../MuteButton/MuteButton'
 import styles from './SideNav.module.css'
 
-function SideNav() {
+function SideNav({ streamRef }) {
 
   return (
     <nav className={styles.sideNav}>
-    
+      <EditorButton />
+      <MuteButton streamRef={streamRef} />
     </nav>
   )
 }
