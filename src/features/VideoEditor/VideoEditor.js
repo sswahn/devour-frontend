31670c867612a39fb734fb7ce88ed4bece095d81
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-04 09:08:40 -0400

    Update VideoEditor.js

--- src/features/VideoEditor/VideoEditor.js
+++ src/features/VideoEditor/VideoEditor.js
@@ -1,14 +1,28 @@
+import { useState, useEffect } from 'react'
 import useFootage from '../../hooks/useFootage'
 import styles from './VideoEditor.module.css'
 
 function VideoEditor() {
   const { footage, duration } = useFootage()
+  const [source, setSource] = useState('')
+
+  const combineFootage = () => {
+    const combinedBlob = new Blob(footage, { type: 'video/webm' })
+    const videoUrl = URL.createObjectURL(combinedBlob)
+    setSource(videoUrl)
+  }
+
+  useEffect(() => {
+    if (!source) {
+      combineFootage()
+    }
+  }, [])
   
   return (
     <section className={styles.videoEditor}>
       <div>
 
-        <video src={videoUrl} preload="metadata" muted playsInline loop />
+        <video src={source} />
     
       </div>
     </section>
