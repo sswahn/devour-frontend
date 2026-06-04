571a067e035945ff09b745c63be7a0c4e94a7613
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-04 15:08:11 -0400

    Update LightButton.js

--- src/features/Camera/LightButton/LightButton.js
+++ src/features/Camera/LightButton/LightButton.js
@@ -7,13 +7,14 @@ import styles from './LightButton.module.css'
 function LightButton({ streamRef }) {
   const [light, setLight] = useState(false)
 
-  const handleTurnOnLight = event => {
+  const onClick = event => {
+    navigator.vibrate?.(50)
     light ? camera.dark(streamRef.current) : camera.light(streamRef.current)
     setLight(!light)
   }
   
   return (
-    <button className={styles.lightButton} onClick={handleTurnOnLight} type="button" aria-label="camera light button">
+    <button className={styles.lightButton} onClick={onClick} type="button" aria-label="camera light button">
       {light ? <DarkSunIcon /> : <SunIcon />}
     </button>
   )
