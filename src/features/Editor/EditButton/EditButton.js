26245192399292312c7901addd40959650fcf48d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 09:08:11 -0400

    Create EditButton.js

---
+++ src/features/Editor/EditButton/EditButton.js
@@ -0,0 +1,13 @@
+
+import styles from './EditButton.module.css'
+
+function EditButton() {
+
+  return (
+    <button className={styles.editButton} type="button">
+  
+    </button>
+  )
+}
+
+export default EditButton
