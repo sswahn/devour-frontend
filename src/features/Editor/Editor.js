a06e5e0ce46d82a3068dce2725dfa90c84e316de
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 07:20:03 -0400

    Update Editor.js

--- src/features/Editor/Editor.js
+++ src/features/Editor/Editor.js
@@ -26,10 +26,33 @@ function Editor() {
       URL.revokeObjectURL(url.current)
     }
   }, [])
+
+  // should be single buttons
   
   return (
     <section className={styles.editor}>
-
+      {pause ? ( 
+        <button className="icon-btn-alt" onClick={handlePlayVideo} type="button" aria-label="play video" aria-controls={`video-preview-${index}`}>
+          <PlayIcon />
+          <div className="tooltip" role="tooltip">Play</div>
+        </button>
+      ) : ( 
+        <button className="icon-btn-alt" onClick={handlePauseVideo} type="button" aria-label="pause video" aria-controls={`video-preview-${index}`}>
+          <PauseIcon />
+          <div className="tooltip" role="tooltip">Pause</div>
+        </button>
+      )}
+      {mute ? (
+        <button className="icon-btn-alt" onClick={handleToggleMute} type="button" aria-label="unmute video" aria-controls={`video-preview-${index}`}>
+          <VolumeXIcon />
+          <div className="tooltip" role="tooltip">Unmute</div>
+        </button>
+      ) : (
+        <button className="icon-btn-alt" onClick={handleToggleMute} type="button" aria-label="mute video" aria-controls={`video-preview-${index}`}> 
+          <VolumeHighIcon />
+          <div className="tooltip" role="tooltip">Mute</div>
+        </button>
+      )}
       <video src={source} onPlay={onPlay} onPause={onPause}  loop playsinline />
     
     </section>
