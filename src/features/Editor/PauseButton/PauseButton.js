8402aa1d333b87f4242991adb4b270db863ea1ae
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 07:58:14 -0400

    Update PauseButton.js

--- src/features/Editor/PauseButton/PauseButton.js
+++ src/features/Editor/PauseButton/PauseButton.js
@@ -1,9 +1,15 @@
+import PlayIcon from '../../../components/Icons/PlayIcon/PlayIcon'
+import PauseIcon from '../../../components/Icons/PauseIcon/PauseIcon'
 import styles from './PauseButton.module.css'
 
-function PauseButton() {
+function PauseButton({ pause }) {
+
+  
 
   return (
-    
+    <button className="icon-btn-alt" onClick={togglePause} type="button" aria-label={`${pause ? 'play' : 'pause'} video`}>
+      {pause ? <PlayIcon /> : <PauseIcon />}
+    </button>
   )
 }
 
