a32b22229134f344787c125a48b04bcfe1645800
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 08:10:48 -0400

    Update MuteButton.js

--- src/features/Editor/MuteButton/MuteButton.js
+++ src/features/Editor/MuteButton/MuteButton.js
@@ -5,7 +5,10 @@ import styles from './MuteButton.module.css'
 function MuteButton() {
   const [mute, setMute] = useState(false)
 
-  
+  const toggleMute = event => {
+    mute ? videoRefs.current.play() : videoRefs.current.pause()
+    setPause(prev => !prev) 
+  }
 
   return (
     <button className="icon-btn-alt" onClick={toggleMute} type="button" aria-label="unmute video" aria-controls={`video-preview-${index}`}>
