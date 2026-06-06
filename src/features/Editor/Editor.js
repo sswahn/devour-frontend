09c80e75e353c77634c94441779d38bb8fd33cc3
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 08:10:06 -0400

    Update Editor.js

--- src/features/Editor/Editor.js
+++ src/features/Editor/Editor.js
@@ -1,11 +1,11 @@
 import { useState, useRef, useEffect } from 'react'
 import useFootage from '../../hooks/useFootage'
+import PauseButton from './PauseButton/PauseButton'
+import MuteButton from './MuteButton/MuteButton'
 import styles from './Editor.module.css'
 
 function Editor() {
   const { footage, duration } = useFootage()
-  const [pause, setPause] = useState(false)
-  const [mute, setMute] = useState(false)
   const [source, setSource] = useState('')
   const url = useRef(null)
   const videoRef = useRef(null)
@@ -19,8 +19,6 @@ function Editor() {
 
   const onPlay = event => {}
   const onPause = event => {}
-  const togglePause = event => {}
-  const toggleMute = event => {}
 
   useEffect(() => {
     combineFootage()
@@ -33,23 +31,9 @@ function Editor() {
   
   return (
     <section className={styles.editor}>
-      {pause &&
-        <button className="icon-btn-alt" onClick={togglePause} type="button" aria-label={`${pause ? 'play' : 'pause'} video`}>
-          {pause ? <PlayIcon /> : <PauseIcon />}
-        </button>
-      }
-      {mute ? (
-        <button className="icon-btn-alt" onClick={toggleMute} type="button" aria-label="unmute video" aria-controls={`video-preview-${index}`}>
-          {mute ? <VolumeXIcon /> : <VolumeHighIcon />}
-        </button>
-      ) : (
-        <button className="icon-btn-alt" onClick={handleToggleMute} type="button" aria-label="mute video" aria-controls={`video-preview-${index}`}> 
-          
-          <div className="tooltip" role="tooltip">Mute</div>
-        </button>
-      )}
+      <PauseButton videoRef={videoRef} />
+      <MuteButton videoRef={videoRef} />
       <video ref={videoRef} src={source} onPlay={onPlay} onPause={onPause} loop playsinline />
-    
     </section>
   )
 }
