c78f6ce2495d0693ccb24edce60f57ab74b9b167
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 06:37:55 -0400

    Update SideNav.js

--- src/features/Camera/SideNav/SideNav.js
+++ src/features/Camera/SideNav/SideNav.js
@@ -2,14 +2,14 @@ import useFootage from '../../../hooks/useFootage'
 import EditorButton from '../EditorButton/EditorButton'
 import styles from './SideNav.module.css'
 
-function SideNav({ streamRef }) {
+function SideNav({ setEditorIsOpen }) {
   const { footage } = useFootage()
   
   // conditionally render EditorButton with !!footage.length
   
   return (
     <nav className={styles.sideNav}>
-      <EditorButton />
+      <EditorButton setEditorIsOpen={setEditorIsOpen} />
     </nav>
   )
 }
