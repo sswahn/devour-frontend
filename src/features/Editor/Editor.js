70ab893c7bc9b0adc3088acc506f3920b81953a6
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 07:26:49 -0400

    Add pause and mute functionality to Editor

--- src/features/Editor/Editor.js
+++ src/features/Editor/Editor.js
@@ -4,21 +4,22 @@ import styles from './Editor.module.css'
 
 function Editor() {
   const { footage, duration } = useFootage()
+  const [pause, setPause] = useState(false)
+  const [mute, setMute] = useState(false)
   const [source, setSource] = useState('')
   const url = useRef(null)
 
   const combineFootage = () => {
     const combinedBlob = new Blob(footage, { type: 'video/webm' })
     const videoUrl = URL.createObjectURL(combinedBlob)
-    
-    alert('videoUrl: ' + videoUrl)
-    
     url.current = videoUrl
     setSource(videoUrl)
   }
 
   const onPlay = event => {}
   const onPause = event => {}
+  const togglePause = event => {}
+  const toggleMute = event => {}
 
   useEffect(() => {
     combineFootage()
@@ -31,25 +32,18 @@ function Editor() {
   
   return (
     <section className={styles.editor}>
-      {pause ? ( 
-        <button className="icon-btn-alt" onClick={handlePlayVideo} type="button" aria-label="play video" aria-controls={`video-preview-${index}`}>
-          <PlayIcon />
-          <div className="tooltip" role="tooltip">Play</div>
+      {pause &&
+        <button className="icon-btn-alt" onClick={togglePause} type="button" aria-label={`${pause ? 'play' : 'pause'} video`}>
+          {pause ? <PlayIcon /> : <PauseIcon />}
         </button>
-      ) : ( 
-        <button className="icon-btn-alt" onClick={handlePauseVideo} type="button" aria-label="pause video" aria-controls={`video-preview-${index}`}>
-          <PauseIcon />
-          <div className="tooltip" role="tooltip">Pause</div>
-        </button>
-      )}
+      }
       {mute ? (
-        <button className="icon-btn-alt" onClick={handleToggleMute} type="button" aria-label="unmute video" aria-controls={`video-preview-${index}`}>
-          <VolumeXIcon />
-          <div className="tooltip" role="tooltip">Unmute</div>
+        <button className="icon-btn-alt" onClick={toggleMute} type="button" aria-label="unmute video" aria-controls={`video-preview-${index}`}>
+          {mute ? <VolumeXIcon /> : <VolumeHighIcon />}
         </button>
       ) : (
         <button className="icon-btn-alt" onClick={handleToggleMute} type="button" aria-label="mute video" aria-controls={`video-preview-${index}`}> 
-          <VolumeHighIcon />
+          
           <div className="tooltip" role="tooltip">Mute</div>
         </button>
       )}
