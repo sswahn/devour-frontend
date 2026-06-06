15ab1e9bcc3e93bf3208420f34831269026c32ad
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 13:56:27 -0400

    Update TopNav.js

--- src/features/Editor/TopNav/TopNav.js
+++ src/features/Editor/TopNav/TopNav.js
@@ -1,15 +1,20 @@
+import { overlay } from '../../../config'
 import useSession from '../../../hooks/useSession'
 import BackButton from '../../../components/BackButton/BackButton'
 import PauseButton from '../PauseButton/PauseButton'
 import MuteButton from '../MuteButton/MuteButton'
 import styles from './TopNav.module.css'
 
-function TopNav({ videoRef,  }) {
+function TopNav({ videoRef, setEditorIsOpen }) {
   const { session } = useSession()
+
+  const closeEditor = () => {
+    setEditorIsOpen(false)
+  }
   
   return (
     <nav className={styles.topNav}>
-      <BackButton overlay={} close={} />
+      <BackButton overlay={overlay.editor} close={closeEditor} />
       <div>
         <PauseButton videoRef={videoRef} />
         <MuteButton videoRef={videoRef} />
