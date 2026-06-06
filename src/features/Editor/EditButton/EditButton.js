3d10f276476c7576be4ca991f844b0af1a33c1cc
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 09:10:35 -0400

    Update EditButton.js

--- src/features/Editor/EditButton/EditButton.js
+++ src/features/Editor/EditButton/EditButton.js
@@ -1,11 +1,11 @@
-
+import PenToSquareIcon from '../../../components/Icons/PenToSquareIcon/PenToSquareIcon'
 import styles from './EditButton.module.css'
 
 function EditButton() {
 
   return (
-    <button className={styles.editButton} type="button">
-  
+    <button className={styles.editButton} type="button" aria-label="open editor options">
+      <PenToSquareIcon />
     </button>
   )
 }
