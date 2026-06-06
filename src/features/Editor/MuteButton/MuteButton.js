92dc635b166644031f345ae8501c4d4c87c5eee7
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 08:07:51 -0400

    Implement mute functionality in MuteButton
    Add mute state management to MuteButton component.

--- src/features/Editor/MuteButton/MuteButton.js
+++ src/features/Editor/MuteButton/MuteButton.js
@@ -1,10 +1,16 @@
+import { useState } from 'react'
 import styles from './MuteButton.module.css'
 
 
 function MuteButton() {
+  const [mute, setMute] = useState(false)
+
+  
 
   return (
-    
+    <button className="icon-btn-alt" onClick={toggleMute} type="button" aria-label="unmute video" aria-controls={`video-preview-${index}`}>
+      {mute ? <VolumeXIcon /> : <VolumeHighIcon />}
+    </button>
   )
 }
 
