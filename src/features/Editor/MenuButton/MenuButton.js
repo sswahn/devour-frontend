4bbd1cfa0b6aafe137c8f88b13a7c01f55424a10
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 09:26:47 -0400

    Update and rename EditButton.js to MenuButton.js

---
+++ src/features/Editor/MenuButton/MenuButton.js
@@ -0,0 +1,17 @@
+import BarsIcon from '../../../components/Icons/BarsIcon/BarsIcon'
+import styles from './MenuButton.module.css'
+
+function MenuButton() {
+
+  const onClick = event => {
+    // open editor menu
+  }
+
+  return (
+    <button className={styles.menuButton} onClick={onClick} type="button" aria-label="open editor menu">
+      <BarsIcon />
+    </button>
+  )
+}
+
+export default MenuButton
