be7b6f0c286fa324b3a90f668964632a5f6c6ee6
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-06 08:31:23 -0400

    Update TopNav.js

--- src/features/Editor/TopNav/TopNav.js
+++ src/features/Editor/TopNav/TopNav.js
@@ -1,7 +1,7 @@
 import useSession from '../../../../hooks/useSession'
 import Avatar from '../../../../components/Avatar/Avatar'
-import PauseButton from './PauseButton/PauseButton'
-import MuteButton from './MuteButton/MuteButton'
+import PauseButton from '../PauseButton/PauseButton'
+import MuteButton from '../MuteButton/MuteButton'
 import styles from './TopNav.module.css'
 
 function TopNav({ videoRef }) {
