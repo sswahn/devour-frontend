f25720d17ae5ed8b99c83d39c10257dbba25cd91
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-07 07:10:35 -0400

    Update Editor.js

--- src/features/Editor/Editor.js
+++ src/features/Editor/Editor.js
@@ -2,6 +2,7 @@ import { useState, useRef, useEffect } from 'react'
 import useFootage from '../../hooks/useFootage'
 import TopNav from './TopNav/TopNav'
 import MenuButton from './MenuButton/MenuButton'
+import ProgressBar from './ProgressBar/ProgressBar'
 import styles from './Editor.module.css'
 
 function Editor({ setEditorIsOpen }) {
@@ -28,6 +29,7 @@ function Editor({ setEditorIsOpen }) {
     <section className={styles.editor}>
       <TopNav videoRef={videoRef} setEditorIsOpen={setEditorIsOpen} />
       <MenuButton />
+      <ProgressBar videoRef={videoRef} />
       <video id="video-editor" ref={videoRef} src={source} loop playsinline />
     </section>
   )
