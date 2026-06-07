e7bed219d8842086e06ab0aee73c813718e53f9b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-07 06:50:41 -0400

    Create ProgressBar.js

---
+++ src/features/Editor/ProgressBar/ProgressBar.js
@@ -0,0 +1,12 @@
+import styles from './ProgressBar.module.css'
+
+function ProgressBar() {
+
+  return (
+    <div id="progressContainer" className="progress-container">
+      <div id="progressBar" className="progress-bar"></div>
+    </div>
+  )
+}
+
+export default ProgressBar
