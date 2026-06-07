b0a0e310dc28db16a4125bbf0f45e225bc192f0e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-07 09:46:02 -0400

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import { useState,  Suspense, lazy } from 'react'
+import { useState, useEffect, Suspense, lazy } from 'react'
 import database from '../../utilities/database' // temp to test frontend
 import Suggestions from '../Suggestions/Suggestions'
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
