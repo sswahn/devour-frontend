c25bf3f62ccd8a90f856e2fb6d182a3a126373fb
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-07 14:44:07 -0400

    Create CloseButton.js

---
+++ src/components/CloseButton/CloseButton.js
@@ -0,0 +1,20 @@
+import XmarkIcon from '../Icons/XmarkIcon/XmarkIcon'
+import styles from './CloseButton.module.css'
+
+// make to a global CloseButton
+
+function CloseButton({ overlay, close }) {
+
+  const onClick = event => {
+    navigator.vibrate?.(50)
+    close()
+  }
+  
+  return (
+    <button className={styles.closeButton} onClick={onClick} type="button" aria-label={`close ${overlay}`}>
+      <XmarkIcon />
+    </button>
+  )
+}
+
+export default CloseButton
