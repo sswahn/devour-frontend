919e8edc1408c0ba535d6dd867d6a6eeb15236e7
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-07 15:03:20 -0400

    Update ProgressBar.js

--- src/features/Editor/ProgressBar/ProgressBar.js
+++ src/features/Editor/ProgressBar/ProgressBar.js
@@ -5,6 +5,7 @@ function ProgressBar({ videoRef }) {
   const progressBarRef = useRef(null)
 
   const onClick = event => {
+    navigator.vibrate?.(50)
     const rect = event.currentTarget.getBoundingClientRect()
     const clickX = event.clientX - rect.left // Click position inside the bar
     videoRef.current.currentTime = (clickX / rect.width) * video.duration // Calculate new video time and apply it
