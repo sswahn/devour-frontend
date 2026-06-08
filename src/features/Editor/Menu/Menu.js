1ca0849e8d592411379d95e8100b7c6d0be2c2bd
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-08 07:53:42 -0400

    Update Menu.js

--- src/features/Editor/Menu/Menu.js
+++ src/features/Editor/Menu/Menu.js
@@ -4,22 +4,26 @@ import styles from './Menu.module.css'
 
 function Menu({ closeMenu }) {
   const listRef = useRef(null)
+
+  const onClick = event => {
+    console.log('button clicked.')
+  }
   
   return (
     <div className={styles.menu} aria-label="menu">
       <CloseButton overlay="menu" close={closeMenu} />
       <ul ref={listRef}>
         <li>
-          <button type="button">Location</button>
+          <button onClick={onClick} type="button">Location</button>
         </li>
         <li>
-          <button type="button">Caption</button>
+          <button onClick={onClick} type="button">Caption</button>
         </li>
         <li>
-          <button type="button">Description</button>
+          <button onClick={onClick} type="button">Description</button>
         </li>
         <li>
-          <button type="button">Media Editor</button>
+          <button onClick={onClick} type="button">Media Editor</button>
         </li>
       </ul>
     </div>
