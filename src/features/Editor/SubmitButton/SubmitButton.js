9538ad8976dfb60963b2b89b2046fb8e5931010b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-09 06:33:48 -0400

    Create SubmitButton.js

---
+++ src/features/Editor/SubmitButton/SubmitButton.js
@@ -0,0 +1,13 @@
+import PaperAirplaneIcon from '../../../components/Icons/PaperAirplaneIcon/PaperAirplaneIcon'
+import styles from './SubmitButton.module.css'
+
+function SubmitButton() {
+
+  return (
+    <button type="button">
+    
+    </button>
+  )
+}
+
+export default SubmitButton
