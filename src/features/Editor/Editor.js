c1ca375308ca3f96374aa81a7e64d22c43a343a2
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-09 09:42:00 -0400

    Update Editor.js

--- src/features/Editor/Editor.js
+++ src/features/Editor/Editor.js
@@ -9,6 +9,7 @@ import styles from './Editor.module.css'
 
 function Editor({ openCamera, openPublisher }) {
   const { footage } = useFootage()
+  
   const [menuIsOpen, setMenuIsOpen] = useState(false)
   const [source, setSource] = useState('')
   const [data, setData] = useState({
@@ -16,6 +17,8 @@ function Editor({ openCamera, openPublisher }) {
   })
   const videoRef = useRef(null)
 
+  /*
+  
   const openMenu = () => {
     setMenuIsOpen(true)
   }
@@ -37,7 +40,7 @@ function Editor({ openCamera, openPublisher }) {
     //loadFromStorage()
   }, [])
   
-/*
+
   useEffect(() => {
     const blob = footage || new Blob()
     const videoUrl = URL.createObjectURL(blob) // pass footage directly, after testing complete
@@ -50,10 +53,13 @@ function Editor({ openCamera, openPublisher }) {
 
   return (
     <section className={styles.editor}>
-      <TopNav videoRef={videoRef} openCamera={openCamera} />
+  {/*
+      <TopNav videoRef={videoRef} openCamera={openCamera} /> 
+    
       <ProgressBar videoRef={videoRef} />
       {menuIsOpen && <Menu data={data} setData={setData} closeMenu={closeMenu} />}
-      <MenuButton openMenu={openMenu} />
+  
+      <MenuButton openMenu={openMenu} /> */}
       <SubmitButton openPublisher={openPublisher} />
       <video id="video-editor" ref={videoRef} src={source} loop playsinline />
     </section>
