2b744653b44e46a946b132e374deb32cc06c1141
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-09 06:35:43 -0400

    Update SubmitButton.js

--- src/features/Editor/SubmitButton/SubmitButton.js
+++ src/features/Editor/SubmitButton/SubmitButton.js
@@ -1,11 +1,15 @@
-import PaperAirplaneIcon from '../../../components/Icons/PaperAirplaneIcon/PaperAirplaneIcon'
+import PaperPlaneIcon from '../../../components/Icons/PaperPlaneIcon/PaperPlaneIcon'
 import styles from './SubmitButton.module.css'
 
 function SubmitButton() {
 
+  const onClick = event => {
+    // open Add text overlay
+  }
+
   return (
-    <button type="button">
-    
+    <button onClick={onClick} type="button" aria-label="submit video">
+      <PaperPlaneIcon />
     </button>
   )
 }
