d2b13b3e6eb7269b4d7a233a9290d802201d5b60
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-11 09:26:16 -0400

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import CloseButton from './CloseButton/CloseButton'
+import CloseButton from '../CloseButton/CloseButton'
 import styles from './Sidebar.module.css'
 
 // consider removing, global sidebar not needed.
@@ -26,7 +26,7 @@ function Sidebar({ sidebarRef, content, close }) {
   return (
     <aside className={styles.overlay} ref={sidebarRef} onClick={onClick}>
       <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
-        <CloseButton close={close} />
+        <CloseButton overlay="sidebar" close={close} />
         {content}
       </div>
     </aside>
