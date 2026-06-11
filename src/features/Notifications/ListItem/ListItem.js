f5ea49c51afbc2a009215e8adf7e66f8bb341778
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-11 12:59:05 -0400

    Update ListItem.js

--- src/features/Notifications/ListItem/ListItem.js
+++ src/features/Notifications/ListItem/ListItem.js
@@ -1,4 +1,6 @@
-import Avatar from '../../Avatar/Avatar'
+import Avatar from '../../../components/Avatar/Avatar'
+import FlagIcon from '../../../components/Icons/FlagIcon/FlagIcon'
+import XmarkIcon from '../../../components/Icons/UserXmarkIcon/UserXmarkIcon'
 import styles from './ListItem.module.css'
 
 function ListItem({ notification }) {
