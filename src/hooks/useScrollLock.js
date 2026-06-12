c20e3eba49471c4e1fcb7b14deaa2b8a653fb2c4
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-12 08:58:38 -0400

    Update useScrollLock.js

--- src/hooks/useScrollLock.js
+++ src/hooks/useScrollLock.js
@@ -8,7 +8,8 @@ function useScrollLock(isActive) {
     const root = document.getElementById('root')
     root.classList.add('lockScroll')
     return () => {
-      root.removeAttribute('class')
+      // root.removeAttribute('class')
+      root.classList.remove('lockScroll') // test to see if leaves empty class attribute
       window.scrollTo(0, scrollY)
     }
   }
