7d6fd88710cd87b9f2f0fadaf0f562a341484bdd
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-12 12:45:33 -0400

    Update server.js

--- src/utilities/server.js
+++ src/utilities/server.js
@@ -3,10 +3,10 @@ const typeCheck = (method, api, request = undefined, headers = {}) => {
   if (typeof api !== 'string') {
     throw new TypeError(`${method} request expects first argument to be a string.`, { cause: 'server' })
   }
-  if (request && (typeof request !== 'object' || Array.isArray(request))) {
+  if (typeof request !== 'object' || Array.isArray(request)) {
     throw new TypeError(`${method} request expects second argument to be an object literal.`, { cause: 'server' })
   }
-  if (headers && (typeof headers !== 'object' || Array.isArray(headers))) {
+  if (typeof headers !== 'object' || Array.isArray(headers)) {
     throw new TypeError(`${method} request expects headers to be an object literal.`, { cause: 'server' })
   }
 }
