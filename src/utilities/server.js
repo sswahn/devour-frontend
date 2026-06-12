2c3fa2a7102817cc4df31181567e2aae903ad5cc
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-12 12:19:28 -0400

    Update server.js

--- src/utilities/server.js
+++ src/utilities/server.js
@@ -1,101 +1,80 @@
-import cache from './cache'
 
 const typeCheck = (method, api, request = undefined, headers = {}) => {
   if (typeof api !== 'string') {
-    throw new TypeError(`${method} request expects first argument to be of type string.`)
+    throw new TypeError(`${method} request expects first argument to be of type string.`, { cause: 'server' })
   }
   if (request && (typeof request !== 'object' || Array.isArray(request))) {
-    throw new TypeError(`${method} request expects second argument to be of type object literal.`)
+    throw new TypeError(`${method} request expects second argument to be of type object literal.`, { cause: 'server' })
   }
   if (headers && (typeof headers !== 'object' || Array.isArray(headers))) {
-    throw new TypeError(`${method} request expects headers to be an object literal.`)
+    throw new TypeError(`${method} request expects headers to be an object literal.`, { cause: 'server' })
   }
 }
 
 const handleResponse = async response => {
   if (!response.ok) {
     const errorMessage = await response.text()
-    throw new Error(`${response.status}: ${errorMessage}`)
+    throw new Error(`${response.status}: ${errorMessage}`, { cause: 'server' })
   }
   // Handle 204 No Content or empty body
   if (response.status === 204 || response.headers.get('Content-Length') === '0') {
+    console.log('server response: No Content: ', response)
     return { success: true, message: 'No content' }
   }
   // Handle API response errors
   if (response.error) {
-    throw new Error(response.error)
+    throw new Error(response.error, { cause: 'server' })
   }
   return response.json()
 }
 
 const server = {
   async get(api, headers = {}) {
-    try {
-      typeCheck('get', api, undefined, headers)
-      const request = new Request(api, {
-        method: 'get',
-        headers: { ...headers }
-      })
-      const cachedResponse = await cache.get('main', request)
-      if (cachedResponse) {
-        return handleResponse(cachedResponse)
-      }
-      const response = await fetch(request)
-      await cache.set('main', request, response)
-      return handleResponse(response)
-    } catch (error) {
-      throw new Error(`Failed to execute GET request. ${error}`)
-    }
+    typeCheck('get', api, undefined, headers)
+    const request = new Request(api, {
+      method: 'get',
+      headers: { ...headers }
+    })
+    const response = await fetch(request)
+    return handleResponse(response)
   },
   async post(api, request, headers = {}) {
-    try {
-      typeCheck('post', api, request, headers)
-      const options = {
-        method: 'post',
-        body: JSON.stringify(request),
-        headers: {
-          'Content-Type': 'application/json',
-          ...headers
-        }
+    typeCheck('post', api, request, headers)
+    const options = {
+      method: 'post',
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers
       }
-      const response = await fetch(api, options)
-      return handleResponse(response)
-    } catch (error) {
-      throw new Error(`Failed to execute POST request. ${error}`)
     }
+    const response = await fetch(api, options)
+    return handleResponse(response)
   },
   async put(api, request, headers = {}) {
-    try {
-      typeCheck('put', api, request, headers)
-      const options = {
-        method: 'put',
-        body: JSON.stringify(request),
-        headers: {
-          'Content-Type': 'application/json',
-          ...headers
-        }
+    typeCheck('put', api, request, headers)
+    const options = {
+      method: 'put',
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers
       }
-      const response = await fetch(api, options)
-      return handleResponse(response)
-    } catch (error) {
-      throw new Error(`Failed to execute PUT request. ${error}`)
     }
+    const response = await fetch(api, options)
+    return handleResponse(response)
   },
   async delete(api, headers = {}) {
-    try {
-      typeCheck('delete', api, undefined, headers)
-      const options = {
-        method: 'delete',
-        headers: {
-          'Content-Type': 'application/json',
-          ...headers
-        }
+    typeCheck('delete', api, undefined, headers)
+    const options = {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers
       }
-      const response = await fetch(api, options)
-      return handleResponse(response)
-    } catch (error) {
-      throw new Error(`Failed to execute DELETE request. ${error}`)
     }
+    const response = await fetch(api, options)
+    return handleResponse(response)
   }
 }
 
