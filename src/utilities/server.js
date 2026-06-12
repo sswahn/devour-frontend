d312ea6d88aff5b6ee825e5d5b3b10892887cb4d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-12 12:23:58 -0400

    Update server.js

--- src/utilities/server.js
+++ src/utilities/server.js
@@ -64,7 +64,7 @@ const server = {
     const response = await fetch(api, options)
     return handleResponse(response)
   },
-  async delete(api, headers = {}) { // delete is a keyword, consider changing
+  async remove(api, headers = {}) {
     typeCheck('delete', api, undefined, headers)
     const options = {
       method: 'delete',
