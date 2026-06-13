d39c0ad5c2b0bd34cb8f55995170141443d722bf
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-13 06:40:21 -0400

    Update NextButton.js

--- src/components/SlideShow/NextButton/NextButton.js
+++ src/components/SlideShow/NextButton/NextButton.js
@@ -1,5 +1,18 @@
+import ChevronRightIcon from '../../Icons/ChevronLeftIcon/ChevronLeftIcon'
+import styles from './NextButton.module.css'
 
+function NextButton({ length, setCurrentIndex }) {
 
-function NextButton() {}
+  const onClick = event => {
+    console.log('clicked previous slide')
+    setCurrentIndex(prev => (prev - 1 + length) % length) // data.length
+  }
+  
+  return (
+    <button className={styles.previousButton} onClick={onClick} aria-label="next slide">
+      <ChevronRightIcon />
+    </button>
+  )
+}
 
 export default NextButton
