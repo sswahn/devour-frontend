35287e3c89765f320f727136c46d299229017b2e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-13 06:48:16 -0400

    Update SlideNav.js

--- src/components/SlideShow/SlideNav/SlideNav.js
+++ src/components/SlideShow/SlideNav/SlideNav.js
@@ -2,12 +2,11 @@ import NextButton from '../NextButton/NextButton'
 import PreviousButton from '../PreviousButton/PreviousButton'
 import styles from './SlideNav.module.css'
 
-function SlideNav({ length, setIndex }) {
-
+function SlideNav({ length, setCurrentIndex }) {
   return (
     <nav className={styles.slideNav}>
-      <NextButton />
-      <PreviousButton />
+      <NextButton length={length} setCurrentIndex={setCurrentIndex} />
+      <PreviousButton length={length} setCurrentIndex={setCurrentIndex} />
     </nav>
   )
 }
