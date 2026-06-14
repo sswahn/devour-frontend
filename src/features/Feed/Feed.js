64b90b98896db03c9523ae354810fb1f8d199b8d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-06-14 06:54:17 -0400

    Update Feed.js

--- src/features/Feed/Feed.js
+++ src/features/Feed/Feed.js
@@ -8,7 +8,7 @@ import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner'
 import Sentinel from './Sentinel/Sentinel' 
 import styles from './Feed.module.css'
 
-function Feed({ data, setData }) {
+function Feed({ data, setData, videoRefs, style }) {
   const { observe, unobserve, disconnect } = createObserver()
   const [loadMore, setLoadMore] = useState(false)
   const [loading, setLoading] = useState(false)
@@ -52,8 +52,8 @@ function Feed({ data, setData }) {
   }, [])
   
   return (
-    <section className={styles.feed} role="feed">
-      {data.map((item, index) => <FeedNode key={index} item={item} index={index + 1} count={data.length} />)}
+    <section className={styles.feed} style={style} role="feed">
+      {data.map((item, index) => <FeedNode key={index} ref={videoRefs} item={item} index={index + 1} count={data.length} />)}
       {loading && <LoadingSpinner />}
       <Sentinel setLoadMore={setLoadMore} />
     </section>
